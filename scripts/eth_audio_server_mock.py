371d8f335da6a51a192119515a4f62e7acb0066b
Lincoln Lorenz <lincoln@micro-nova.com>
2020-08-25 17:23:49 -0400

    Move eth_audio to class based api thing
    This will allow us to integrate it into the http server and allow the mock and actual controller to be interchangeable

--- scripts/eth_audio_server_mock.py
+++ scripts/eth_audio_server_mock.py
@@ -144,37 +144,12 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
 
-# TODO: make this a class
-
+# Helper functions
 def encode(pydata):
     return json.dumps(pydata)
 
 def decode(j):
     return json.loads(j)
-
-def test_cmd(cmd):
-    try:
-        command = cmd['command']
-        if command is None:
-            return error('No command specified')
-        elif command is 'return_state':
-            return state()
-        elif command is 'set_power':
-            return set_power(cmd['audio_power'], cmd['usb_power'])
-        elif command is 'set_source':
-            return set_source(cmd['id'], cmd['name'], cmd['digital'])
-        elif command is 'set_zone':
-            return set_zone(cmd['id'], cmd['name'], cmd['source_id'], cmd['mute'], cmd['stby'], cmd['vol'], cmd['disabled'])
-        elif command is 'set_group':
-            return error('set_group unimplemented')
-        elif command is 'create_group':
-            return error('create_group unimplemented')
-        elif command is 'delete_group':
-            return error('delete_group unimplemented')
-        else:
-            return error('command {} is not supported'.format(command))
-    except Exception as e:
-        return error(str(e)) # TODO: handle exception more verbosely
 
 def parse_int(i, options):
     if int(i) in options:
@@ -182,152 +157,178 @@ def parse_int(i, options):
     else:
         raise ValueError('{} is not in [{}]'.format(i, options))
 
-
-# This script emulates the JSON command/response server hosted on an EthAudio box
-status = { # This is the system state response that will come back from the ethaudio box
-    "power": {
-        "audio_power": False, # this needs to be on for any zone to work
-        "usb_power": False     # this turns on/off the usb power port
-    },
-    "sources": [ # this is an array of source objects, each has an id, name, and bool specifying wheater source comes from RCA or digital input
-        { "id": 0, "name": "Source 1", "digital": False  },
-        { "id": 1, "name": "Source 2", "digital": False  },
-        { "id": 2, "name": "Source 3", "digital": False  },
-        { "id": 3, "name": "Source 4", "digital": False  }
-    ],
-    "zones": [ # this is an array of zones, array length depends on # of boxes connected
-        { "id": 0, "name": "Zone 1", "source_id": 0, "mute": False , "stby": False , "disabled": False , "vol": 0 },
-        { "id": 1, "name": "Zone 2", "source_id": 0, "mute": False , "stby": False , "disabled": False , "vol": 0 },
-        { "id": 2, "name": "Zone 3", "source_id": 0, "mute": False , "stby": False , "disabled": False , "vol": 0 },
-        { "id": 3, "name": "Zone 4", "source_id": 0, "mute": False , "stby": False , "disabled": False , "vol": 0 },
-        { "id": 4, "name": "Zone 5", "source_id": 0, "mute": False , "stby": False , "disabled": False , "vol": 0 },
-        { "id": 5, "name": "Zone 6", "source_id": 0, "mute": False , "stby": False , "disabled": False , "vol": 0 }
-    ],
-    "groups": [ # this is an array of groups that have been created , each group has a friendly name and an array of member zones
-        { "id": 0, "name": "Group 1", "zones": [0,1,2] },
-        { "id": 1, "name": "Group 2", "zones": [2,3,4] },
-        { "id": 2, "name": "Group 3", "zones": [5] }
-    ]
-}
-
 def error(msg):
     return encode({'error': msg})
 
-# This command can be used to return the system state
-#{
-#    "command":"return_state"
-#}
-def state():
-    return encode(status)
+# TODO: make this a class
+class EthAudioApi:
 
+    def __init__(self):
+        # This script emulates the JSON command/response server hosted on an EthAudio box
+        self.status = { # This is the system state response that will come back from the ethaudio box
+            "power": {
+                "audio_power": False, # this needs to be on for any zone to work
+                "usb_power": False     # this turns on/off the usb power port
+            },
+            "sources": [ # this is an array of source objects, each has an id, name, and bool specifying wheater source comes from RCA or digital input
+                { "id": 0, "name": "Source 1", "digital": False  },
+                { "id": 1, "name": "Source 2", "digital": False  },
+                { "id": 2, "name": "Source 3", "digital": False  },
+                { "id": 3, "name": "Source 4", "digital": False  }
+            ],
+            "zones": [ # this is an array of zones, array length depends on # of boxes connected
+                { "id": 0, "name": "Zone 1", "source_id": 0, "mute": False , "stby": False , "disabled": False , "vol": 0 },
+                { "id": 1, "name": "Zone 2", "source_id": 0, "mute": False , "stby": False , "disabled": False , "vol": 0 },
+                { "id": 2, "name": "Zone 3", "source_id": 0, "mute": False , "stby": False , "disabled": False , "vol": 0 },
+                { "id": 3, "name": "Zone 4", "source_id": 0, "mute": False , "stby": False , "disabled": False , "vol": 0 },
+                { "id": 4, "name": "Zone 5", "source_id": 0, "mute": False , "stby": False , "disabled": False , "vol": 0 },
+                { "id": 5, "name": "Zone 6", "source_id": 0, "mute": False , "stby": False , "disabled": False , "vol": 0 }
+            ],
+            "groups": [ # this is an array of groups that have been created , each group has a friendly name and an array of member zones
+                { "id": 0, "name": "Group 1", "zones": [0,1,2] },
+                { "id": 1, "name": "Group 2", "zones": [2,3,4] },
+                { "id": 2, "name": "Group 3", "zones": [5] }
+            ]
+        }
 
-# This command can be used to enable / disable the 9V audio power and 5V usb power
-# Along with the command one or more of the parameters can be passed
-# The system state struct will be returned if the command was successfully processed, error response otherwise
-#{
-#    "command":"set_power",
-#    "audio_power": False | True,
-#    "usb_power": False | True
-#}
-def set_power(audio_on, usb_on):
-    status['power']['audio_power'] = audio_on
-    status['power']['usb_power'] = usb_on
-    return state
-
-# This command can be used to modify any of the 4 system sources
-# Along with the command one or more of the parameters can be passed
-# The system state struct will be returned if the command was successfully processed, error response otherwise
-#{
-#    "command":"set_source",
-#    "id":0 | 1 | 2 | 3,
-#    "name":"new name", # sets the friendly name for the source, ie "cd player" or "stream 1"
-#    "digital": False | True # sets the connection for the source, either analog (RCA) or digital (sharpoint)
-#}
-def set_source(id, name, digital):
-    idx = None
-    for i, s in enumerate(status['sources']):
-        if s['id'] == id:
-            idx = i
-    if idx is not None:
+    def test_cmd(self, cmd):
         try:
-            status['sources'][idx]['name'] = str(name)
-            status['sources'][idx]['digital'] = bool(digital)
-            return state()
+            command = cmd['command']
+            if command is None:
+                return error('No command specified')
+            elif command is 'return_state':
+                return self.state()
+            elif command is 'set_power':
+                return self.set_power(cmd['audio_power'], cmd['usb_power'])
+            elif command is 'set_source':
+                return self.set_source(cmd['id'], cmd['name'], cmd['digital'])
+            elif command is 'set_zone':
+                return self.set_zone(cmd['id'], cmd['name'], cmd['source_id'], cmd['mute'], cmd['stby'], cmd['vol'], cmd['disabled'])
+            elif command is 'set_group':
+                return error('set_group unimplemented')
+            elif command is 'create_group':
+                return error('create_group unimplemented')
+            elif command is 'delete_group':
+                return error('delete_group unimplemented')
+            else:
+                return error('command {} is not supported'.format(command))
         except Exception as e:
-            return error('set source ' + str(e))
-    else:
-        return error('set source: index {} out of bounds'.format(idx))
+            return error(str(e)) # TODO: handle exception more verbosely
 
-# This command can be used to modify any zone
-# Along with the command one or more of the parameters can be passed
-# The number of the zones depends on how many boxes are chained together, check system state for # of zones
-# The system state struct will be returned if the command was successfully processed, error response otherwise
-#{
-#    "command":"set_zone",
-#    "id":any valid zone,
-#    "name":"new name" # sets the friendly name for the zone, ie "bathroom" or "kitchen 1"
-#    "source_id": 0 | 1 | 2 | 3
-#    "mute": False | True # this mutes the zone regardless of set volume
-#    "stby": False | True # this sets the zone to standby, very low power consumption
-#    "vol": 0 to -79 # this sets the zone attenuation, 0 is max volume, -79 is min volume
-#    "disabled": False | True # set this to True if the zone is not connected to any speakers and not in use
-#}
-def set_zone(id, name, source_id, mute, stby, vol, disabled):
-    idx = None
-    for i, s in enumerate(status['zones']):
-        if s['id'] == id:
-            idx = i
-    if idx is not None:
-        try:
-            status['zones'][idx]['name'] = str(name)
-            status['zones'][idx]['source_id'] = parse_int(source_id, [1, 2, 3, 4])
-            status['zones'][idx]['mute'] = bool(mute)
-            status['zones'][idx]['stby'] = bool(stby)
-            status['zones'][idx]['vol'] = parse_int(vol, range(-79, 1))
-            status['zones'][idx]['disabled'] = bool(disabled)
-            return state()
-        except Exception as e:
-            return error('set zone'  + str(e))
-    else:
-        return error('set zone: index {} out of bounds'.format(idx))
+    # This command can be used to return the system state
+    #{
+    #    "command":"return_state"
+    #}
+    def state(self):
+        return encode(self.status)
 
-# TODO: make set group
-# This command can be used to set any EXISTING group
-# Along with the command one or more of the parameters can be passed
-# check the system state for a list of existing group
-# The system state struct will be returned if the command was successfully processed, error response otherwise
-#{
-#    "command":"set_group",
-#    "id":any vaild group,
-#    "name":"new name" # sets the friendly name for the group, ie "upstairs" or "back yard"
-#    "source_id": 0 | 1 | 2 | 3 # change all zones in group to different source
-#    "zones": [0,1,2...] # specify new array of zones that make up the group
-#    "mute": False | True # mutes all zones in group
-#    "stby": False | True # sets all zone in group to standby
-#    "vol_delta": 0 to 79 # CHANGES the volume of each zone in the group by this much. For each zone, will saturate if out of range
-#}
 
-# TODO: make create new group
-# This command can be used to create a NEW group
-# Along with the command ALL parameters must also be passed
-# The system state struct will be returned if the command was successfully processed, error response otherwise
-# Refer to the returned system state to obtain the id for the newly created group
-#{
-#    "command":"create_group"
-#    "name":"new group name"
-#    "zones": [0,1,2...] # specify new array of zones that make up the group
-#}
+    # This command can be used to enable / disable the 9V audio power and 5V usb power
+    # Along with the command one or more of the parameters can be passed
+    # The system state struct will be returned if the command was successfully processed, error response otherwise
+    #{
+    #    "command":"set_power",
+    #    "audio_power": False | True,
+    #    "usb_power": False | True
+    #}
+    def set_power(self, audio_on, usb_on):
+        self.status['power']['audio_power'] = audio_on
+        self.status['power']['usb_power'] = usb_on
 
-# TODO: make delete group
-# This command can be used to delete an EXISTING group
-# Along with the command ALL parameters must also be passed
-# The system state struct will be returned if the command was successfully processed, error response otherwise
-#{
-#    "command":"delete_group"
-#    "id":"new group name"
-#}
+    # This command can be used to modify any of the 4 system sources
+    # Along with the command one or more of the parameters can be passed
+    # The system state struct will be returned if the command was successfully processed, error response otherwise
+    #{
+    #    "command":"set_source",
+    #    "id":0 | 1 | 2 | 3,
+    #    "name":"new name", # sets the friendly name for the source, ie "cd player" or "stream 1"
+    #    "digital": False | True # sets the connection for the source, either analog (RCA) or digital (sharpoint)
+    #}
+    def set_source(self, id, name, digital):
+        idx = None
+        for i, s in enumerate(self.status['sources']):
+            if s['id'] == id:
+                idx = i
+        if idx is not None:
+            try:
+                self.status['sources'][idx]['name'] = str(name)
+                self.status['sources'][idx]['digital'] = bool(digital)
+            except Exception as e:
+                return error('set source ' + str(e))
+        else:
+            return error('set source: index {} out of bounds'.format(idx))
 
-# TODO: end of class
+    # This command can be used to modify any zone
+    # Along with the command one or more of the parameters can be passed
+    # The number of the zones depends on how many boxes are chained together, check system state for # of zones
+    # The system state struct will be returned if the command was successfully processed, error response otherwise
+    #{
+    #    "command":"set_zone",
+    #    "id":any valid zone,
+    #    "name":"new name" # sets the friendly name for the zone, ie "bathroom" or "kitchen 1"
+    #    "source_id": 0 | 1 | 2 | 3
+    #    "mute": False | True # this mutes the zone regardless of set volume
+    #    "stby": False | True # this sets the zone to standby, very low power consumption
+    #    "vol": 0 to -79 # this sets the zone attenuation, 0 is max volume, -79 is min volume
+    #    "disabled": False | True # set this to True if the zone is not connected to any speakers and not in use
+    #}
+    def set_zone(self, id, name, source_id, mute, stby, vol, disabled):
+        idx = None
+        for i, s in enumerate(self.status['zones']):
+            if s['id'] == id:
+                idx = i
+        if idx is not None:
+            try:
+                self.status['zones'][idx]['name'] = str(name)
+                self.status['zones'][idx]['source_id'] = parse_int(source_id, [1, 2, 3, 4])
+                self.status['zones'][idx]['mute'] = bool(mute)
+                self.status['zones'][idx]['stby'] = bool(stby)
+                self.status['zones'][idx]['vol'] = parse_int(vol, range(-79, 1))
+                self.status['zones'][idx]['disabled'] = bool(disabled)
+            except Exception as e:
+                return error('set zone'  + str(e))
+        else:
+            return error('set zone: index {} out of bounds'.format(idx))
+
+    # TODO: make set group
+    # This command can be used to set any EXISTING group
+    # Along with the command one or more of the parameters can be passed
+    # check the system state for a list of existing group
+    # The system state struct will be returned if the command was successfully processed, error response otherwise
+    #{
+    #    "command":"set_group",
+    #    "id":any vaild group,
+    #    "name":"new name" # sets the friendly name for the group, ie "upstairs" or "back yard"
+    #    "source_id": 0 | 1 | 2 | 3 # change all zones in group to different source
+    #    "zones": [0,1,2...] # specify new array of zones that make up the group
+    #    "mute": False | True # mutes all zones in group
+    #    "stby": False | True # sets all zone in group to standby
+    #    "vol_delta": 0 to 79 # CHANGES the volume of each zone in the group by this much. For each zone, will saturate if out of range
+    #}
+
+    # TODO: make create new group
+    # This command can be used to create a NEW group
+    # Along with the command ALL parameters must also be passed
+    # The system state struct will be returned if the command was successfully processed, error response otherwise
+    # Refer to the returned system state to obtain the id for the newly created group
+    #{
+    #    "command":"create_group"
+    #    "name":"new group name"
+    #    "zones": [0,1,2...] # specify new array of zones that make up the group
+    #}
+
+    # TODO: make delete group
+    # This command can be used to delete an EXISTING group
+    # Along with the command ALL parameters must also be passed
+    # The system state struct will be returned if the command was successfully processed, error response otherwise
+    #{
+    #    "command":"delete_group"
+    #    "id":"new group name"
+    #}
+
+# Temporary placmemnt until we finish testing
+eth_audio = EthAudioApi()
+last_status = deepcopy(eth_audio.state())
 
 test_cmds = [
 {
@@ -381,8 +382,6 @@ test_cmds = [
 #},
 ]
 
-last_status = deepcopy(status)
-
 # pretty print deepdiff's @field name
 def pretty_field(field):
     return str(field).replace("root['", "").replace("']","").replace("['", ".")
@@ -391,7 +390,7 @@ def pretty_field(field):
 #   we use this for debugging
 def show_change():
     global last_status
-    diff = deepdiff.DeepDiff(last_status, status)
+    diff = deepdiff.DeepDiff(last_status, eth_audio.status, ignore_order=True)
     if any(k in diff for k in ('values_changed', 'dictionary_item_added', 'dictionary_item_removed')):
         print('changes:')
         if 'values_changed' in diff:
@@ -405,36 +404,39 @@ def show_change():
                 print('added {}'.format(pretty_field(field)))
     else:
         print('no change!')
-    last_status = deepcopy(status)
+    last_status = deepcopy(eth_audio.status)
 
 if __name__ == "__main__":
+
     # Test emulated commands
-    print(state())
+    print('intial state:')
+    print(eth_audio.state())
+    print('testing commands:')
     show_change()
-    set_power(audio_on=False, usb_on=True)
+    eth_audio.set_power(audio_on=False, usb_on=True)
     show_change()
-    set_source(0, 'Spotify', True)
+    eth_audio.set_source(0, 'Spotify', True)
     show_change()
-    set_source(1, 'Pandora', True)
+    eth_audio.set_source(1, 'Pandora', True)
     show_change()
-    set_source(2, 'TV', False)
+    eth_audio.set_source(2, 'TV', False)
     show_change()
-    set_source(3, 'PC', False)
+    eth_audio.set_source(3, 'PC', False)
     show_change()
-    set_zone(0, 'Party Zone', 1, False, False, 0, False)
+    eth_audio.set_zone(0, 'Party Zone', 1, False, False, 0, False)
     show_change()
-    set_zone(1, 'Drone Zone', 2, False, False, -20, False)
+    eth_audio.set_zone(1, 'Drone Zone', 2, False, False, -20, False)
     show_change()
-    set_zone(2, 'Sleep Zone', 3, True, False, -40, False)
+    eth_audio.set_zone(2, 'Sleep Zone', 3, True, False, -40, False)
     show_change()
-    set_zone(3, "Standby Zone", 4, False, True, -50, False)
+    eth_audio.set_zone(3, "Standby Zone", 4, False, True, -50, False)
     show_change()
-    set_zone(4, "Disabled Zone", 1, False, False, 0, True)
+    eth_audio.set_zone(4, "Disabled Zone", 1, False, False, 0, True)
     show_change()
 
     # Test string/json based command handler
     for cmd in test_cmds:
-        test_cmd(cmd) # TODO: add expected result checker here
+        eth_audio.test_cmd(cmd) # TODO: add expected result checker here
         show_change()
 
 
