7eb54af81cd89156db3453bb72f65fca97f41c53
Lincoln Lorenz <lincoln@micro-nova.com>
2020-08-25 15:42:25 -0400

    Add change printout for sane debugging

--- scripts/eth_audio_server_mock.py
+++ scripts/eth_audio_server_mock.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import json
+from copy import deepcopy
+import deepdiff
 
 # import HTTP server and request handler, threading
 from http.server import HTTPServer, BaseHTTPRequestHandler
@@ -325,25 +327,114 @@ def set_zone(id, name, source_id, mute, stby, vol, disabled):
 #    "id":"new group name"
 #}
 
+test_cmds = [
+{
+    "command" : "set_power",
+    "audio_power" : True,
+    "usb_power" : True
+},
+{
+    "command" : "set_source",
+    "id" : 1,
+    "name" : "cd player",
+    "digital": False
+},
+{
+    "command" : "set_zone",
+    "id" : 2,
+    "name" : "whole house",
+    "source_id" : 2,
+    "mute" : False,
+    "stby" : False,
+    "vol" : -9,
+    "disabled" : False
+},{
+    "command" : "set_zone",
+    "id" : 2,
+    "name" : "whole house",
+    "source_id" : 2,
+    "mute" : False,
+    "stby" : False,
+    "vol" : -9,
+    "disabled" : False
+},
+#{
+#    "command":"set_group",
+#    "id":any vaild group,
+#    "name":"new name" # sets the friendly name for the group, ie "upstairs" or "back yard"
+#    "source_id": 0 | 1 | 2 | 3 # change all zones in group to different source
+#    "zones": [0,1,2...] # specify new array of zones that make up the group
+#    "mute": False | True # mutes all zones in group
+#    "stby": False | True # sets all zone in group to standby
+#    "vol_delta": 0 to 79 # CHANGES the volume of each zone in the group by this much. For each zone, will saturate if out of range
+#},
+#{
+#    "command":"create_group"
+#    "name":"new group name"
+#    "zones": [0,1,2...] # specify new array of zones that make up the group
+#},
+#{
+#    "command":"delete_group"
+#    "id":"new group name"
+#},
+]
+
+last_status = deepcopy(status)
+
+# pretty print deepdiff's @field name
+def pretty_field(field):
+    return str(field).replace("root['", "").replace("']","").replace("['", ".")
+
+# show the difference between status when this was last called
+#   we use this for debugging
+def show_change():
+    global last_status
+    diff = deepdiff.DeepDiff(last_status, status)
+    if any(k in diff for k in ('values_changed', 'dictionary_item_added', 'dictionary_item_removed')):
+        print('changes:')
+        if 'values_changed' in diff:
+            for field, change in diff['values_changed'].items():
+                print('  {} {} -> {}'.format(pretty_field(field), change['old_value'], change['new_value']))
+        if 'dictionary_item_added' in diff:
+            for field in diff['dictionary_item_added']:
+                print('added {}'.format(pretty_field(field)))
+        if 'dictionary_item_removed' in diff:
+            for field in diff['dictionary_item_removed']:
+                print('added {}'.format(pretty_field(field)))
+    else:
+        print('no change!')
+    last_status = deepcopy(status)
+
 if __name__ == "__main__":
     # Test emulated commands
     return_state()
+    show_change()
     set_power(audio_on=False, usb_on=True)
+    show_change()
     set_source(0, 'Spotify', True)
+    show_change()
     set_source(1, 'Pandora', True)
+    show_change()
     set_source(2, 'TV', False)
+    show_change()
     set_source(3, 'PC', False)
+    show_change()
     set_zone(0, 'Party Zone', 1, False, False, 0, False)
+    show_change()
     set_zone(1, 'Drone Zone', 2, False, False, -20, False)
+    show_change()
     set_zone(2, 'Sleep Zone', 3, True, False, -40, False)
+    show_change()
     set_zone(3, "Standby Zone", 4, False, True, -50, False)
+    show_change()
     set_zone(4, "Disabled Zone", 1, False, False, 0, True)
+    show_change()
 
     # Test string/json based command handler
-    test_cmd({
-        "command":"return_state"
-    })
-    # TODO: add more commands
+    for cmd in test_cmds:
+        test_cmd(cmd) # TODO: add expected result checker here
+        show_change()
+
 
     # TODO: Start HTTP server (in new thread)
 
