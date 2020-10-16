8e9968e542f0b8fe7f141312759de606099ef105
Lincoln Lorenz <lincoln@micro-nova.com>
2020-10-16 15:47:56 -0400

    Add mock preamp interface with useful printout
    This is intended to be integrated into the latest interface with a couple of changes

--- python/tests/test_ethaudio_rpi.py
+++ python/tests/test_ethaudio_rpi.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 # this file is expected to be run using pytest, ie. pytest python/tests/test_ethaudio_rpi.py
-
-from .test_ethaudio_mock import check_all_tsts
+if __name__ == '__main__':
+  from test_ethaudio_mock import check_all_tsts
+else:
+  from .test_ethaudio_mock import check_all_tsts
 import ethaudio
 
 def test_rpi():
   check_all_tsts(ethaudio.Api(ethaudio.api.RpiRt()))
+
+if __name__ == '__main__':
+  test_rpi()
