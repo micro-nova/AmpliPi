473d1b3c411f4971b829762d926c9414738339ad
Lincoln Lorenz <lincoln@micro-nova.com>
2020-10-26 12:56:27 -0400

    Add pretty printing of JSON request

--- python/ethaudio/server.py
+++ python/ethaudio/server.py
@@ -6,6 +6,9 @@ import json
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import threading
 
+# pretty printing
+import pprint
+
 # get my ip
 import socket
 
@@ -69,6 +72,9 @@ class EthAudioServer():
         'None' on success, otherwise a json encoded error
     """
     cmd = decode(command_json_text)
+    print("received command: ")
+    pp = pprint.PrettyPrinter(indent=2)
+    pp.pprint(cmd)
     return self.eth_audio_instance.parse_cmd(cmd)
 
   def craft_error(self, error):
