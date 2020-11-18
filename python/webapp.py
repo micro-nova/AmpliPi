9c0dcaf16f17c83c3757c08c9dd8d64ad369435c
Lincoln Lorenz <lincoln@micro-nova.com>
2020-11-18 12:17:02 -0500

    Plumb js functions (input only)

--- python/webapp.py
+++ python/webapp.py
@@ -76,7 +76,7 @@ def options_html(options):
   return '\n'.join(html_options)
 
 def src_html(src):
-  source_header = '<tr><td>{}</td><td><select id="src1_ad" onchange="onSrcChange(this);">'.format(src)
+  source_header = '<tr><td>{}</td><td><select id="s1_input" onchange="onSrcInputChange(this);">'.format(src)
   source_footer = '</select></td></tr>'
   options = {
     "" : "None",
@@ -92,13 +92,13 @@ def group_html(group):
   html_header = '<tr><td>{}</td>'.format(group['name'])
   html = '<td><input id="g{}_vol" type="range" value="{}" onchange="onGroupVolChange(this);" min="-79" max="0"></td>'.format(group['id'], group['vol_delta'])
   html += '<td><span id="g{}_atten">{}</span> dB</td>'.format(group['id'], group['vol_delta'])
-  html += '<td><input type="checkbox" id="g{}_mute" checked="{}"></td>'.format(group['id'], str(group['mute']).lower())
+  html += '<td><input type="checkbox" id="g{}_mute" onchange="onGroupMuteChange(this);" checked="{}"></td>'.format(group['id'], str(group['mute']).lower()) # TODO: add mute/unmute function
   html_footer = '</tr>'
   return html_header + html + html_footer
 
 def unused_groups_html(src):
   header = '<tr><td></td></tr>'
-  html = '<tr><td>Add group</td><td><select id="g_add" onchange="onAddGroupToSrc(this);">'
+  html = '<tr><td>Add group</td><td><select id="s{}_add" onchange="onAddGroupToSrc(this);">'.format(src)
   unused = {g['name'] : g['id'] for g in groups if g['source_id'] != src}
   html += options_html(unused)
   html += '</select></td></tr>'
@@ -111,6 +111,38 @@ def groups_html(src):
   html_groups = ''.join([group_html(group) for group in groups if group['source_id'] == src])
   html_footer = ''
   return html_header + html_groups + html_footer
+
+SCRIPTS = """
+  function onGroupVolChange(obj) {
+    var group = obj.id.substring(0,2);
+    var group_atten = group + "_atten";
+    var gid = group.substring(1,2);
+    document.getElementById(group_atten).innerHTML = obj.value;
+    onRequest(group + ' vol= ' + obj.value);
+  }
+  function onGroupMuteChange(obj) {
+    var group = obj.id.substring(0,2);
+    var mute = obj.checked;
+    var gid = group.substring(1,2);
+    onRequest(group + ' mute= ' + obj.checked);
+  }
+  function onAddGroupToSrc(obj) {
+    var src = obj.id.substring(1,2);
+    var gid = obj.value
+    onRequest('Adding g' + gid  + ' to src ' + src);
+  }
+  function onSrcInputChange(obj) {
+    var input = obj.value
+    var src = obj.id.substring(1,2);
+    onRequest('Changing source ' + src  + ' to use ' + input);
+  }
+  function onRequest(resp) {
+    document.getElementById("request").innerHTML = resp;
+  }
+  function onResponse(resp) {
+    document.getElementById("response").innerHTML = resp;
+  }
+"""
 
 @app.route('/')
 @app.route('/source/<int:src>')
@@ -126,7 +158,13 @@ def index(src=0):
   html += unused_groups_html(src)
   html_footer = '</table></td>'
   html_footer += '<td><button onclick="document.location={}">&gt</button></td>'.format("'/source/{}'".format(nxt))
-  html_footer += '</table></body></html>'
+  html_footer += '</table>'
+  html_footer += '<div>Request:</div>'
+  html_footer += '<div id="request">request goes here</div>'
+  html_footer += '<div>Response:</div>'
+  html_footer += '<div id="response">response goes here</div>'
+  html_footer += '<script>{}</script>'.format(SCRIPTS)
+  html_footer += '</body></html>'
   return html_header + html + html_footer
 
 if __name__ == '__main__':
