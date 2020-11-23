894391e5fc2188c0bc8d305ab5ed2898de54c6ac
Lincoln Lorenz <lincoln@micro-nova.com>
2020-11-23 17:50:22 -0500

    Add start of a templated webpage
    Still needs javascript to populate the contents

--- python/app.py
+++ python/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 import ethaudio
 import json
 from collections import OrderedDict
@@ -63,98 +63,7 @@ def groups_html(src):
   html_footer = ''
   return html_header + html_groups + html_footer
 
-SCRIPTS = """
-  function onGroupVolChange(obj) {
-    var group = obj.id.substring(0,2);
-    var group_atten = group + "_atten";
-    var gid = group.substring(1,2);
-    document.getElementById(group_atten).innerHTML = obj.value;
-    showIntent(group + ' vol= ' + obj.value);
-    req = {
-      "command": "set_group",
-      "id" : Number(gid),
-      "vol_delta" : Number(obj.value),
-      "mute" : false
-    };
-    sendRequest(req)
-  }
-  function onGroupMuteChange(obj) {
-    var group = obj.id.substring(0,2);
-    var mute = obj.checked;
-    var gid = group.substring(1,2);
-    showIntent(group + ' mute= ' + obj.checked);
-    req = {
-      "command": "set_group",
-      "id" : Number(gid),
-      "mute" : Boolean(obj.checked)
-    };
-    sendRequest(req);
-  }
-  //html_header = '<tr><td>{}</td>'.format(group['name'])
-  //html = '<td><input id="g{}_vol" type="range" value="{}" onchange="onGroupVolChange(this);" min="-79" max="0"></td>'.format(group['id'], group['vol_delta'])
-  //html += '<td><span id="g{}_atten">{}</span> dB</td>'.format(group['id'], group['vol_delta'])
-  //html += '<td><input type="checkbox" id="g{}_mute" onchange="onGroupMuteChange(this);" {} ></td>'.format(group['id'], {False:'', True: 'checked'}[group['mute']])
-  //html_footer = '</tr>'
-  function onAddGroupToSrc(obj) {
-    var src = obj.id.substring(1,2);
-    var gid = obj.value
-    showIntent('Adding g' + gid  + ' to src ' + src);
-    req = {
-      "command": "set_group",
-      "id" : Number(gid),
-      "source_id" : Number(src),
-      "mute" : false
-    };
-    sendRequestAndReload(req);
-  }
-  function onSrcInputChange(obj) {
-    var input = obj.value
-    var src = obj.id.substring(1,2);
-    showIntent('Changing source ' + src  + ' to use ' + input);
-    req = {
-      "command": "set_source",
-      "id" : Number(src),
-      "input" : input
-    };
-    sendRequest(req);
-  }
-  async function sendRequest(obj) {
-    onRequest(obj)
-    let response = await fetch('/cmd', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      },
-      body: JSON.stringify(obj)
-    });
-    let result = await response.json();
-    onResponse(result);
-  }
-  async function sendRequestAndReload(obj) {
-    onRequest(obj)
-    let response = await fetch('/cmd', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      },
-      body: JSON.stringify(obj)
-    });
-    let result = await response.json();
-    onResponse(result);
-    window.location.reload(true);
-  }
-  function showIntent(intent) {
-    document.getElementById("intent").innerHTML = intent;
-  }
-  function onRequest(req) {
-    document.getElementById("request").innerHTML = JSON.stringify(req);
-  }
-  function onResponse(resp) {
-    document.getElementById("response").innerHTML = JSON.stringify(resp);
-  }
-"""
-
-@app.route('/cmd', methods=['POST'])
+@app.route('/api', methods=['POST'])
 def parse_cmd():
   req = request.get_json()
   cmd = req.pop('command')
@@ -165,35 +74,15 @@ def parse_cmd():
   else:
     out = {'error': 'Unknown command'}
   print(app.api.visualize_api())
-  if out is None:
-    out = {}
+  if out is None: # None is returned on success
+    out = app.api.get_state()
   return json.dumps(out)
 
 @app.route('/')
 @app.route('/source/<int:src>')
 def index(src=0):
-  print('loading src = {}'.format(src))
-  prv = (src - 1) % 4
-  nxt = (src + 1) % 4
-  html_header ='<html><body><table>'
-  html_header += '<td><button onclick="document.location={}">&lt</button></td>'.format("'/source/{}'".format(prv))
-  html_header += '<td><table id="source_groups">'
-  html = src_html(src)
-  html += groups_html(src)
-  html += unused_groups_html(src)
-  html_footer = '</table></td>'
-  html_footer += '<td><button onclick="document.location={}">&gt</button></td>'.format("'/source/{}'".format(nxt))
-  html_footer += '</table>'
-  html_footer += '<div>-----Debugging-----</div>'
-  html_footer += '<div>Intent:</div>'
-  html_footer += '<div id="intent">intent goes here</div>'
-  html_footer += '<div>Request:</div>'
-  html_footer += '<div id="request">request goes here</div>'
-  html_footer += '<div>Response:</div>'
-  html_footer += '<div id="response">response goes here</div>'
-  html_footer += '<script>{}</script>'.format(SCRIPTS)
-  html_footer += '</body></html>'
-  return html_header + html + html_footer
+  name = app.api.status['sources'][src]['name']
+  return render_template('index.html', src=src, name=name)
 
 def create_app(mock=False, config_file='../config/jasons_house.json'):
   if mock:
