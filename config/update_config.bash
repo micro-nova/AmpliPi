6aefd3bd9fbfb29070049d15e52c1ba4eb924da2
Lincoln Lorenz <lincoln@micro-nova.com>
2021-01-15 10:37:49 -0500

    Fix bug in auto installation of shairport-sync

--- config/update_config.bash
+++ config/update_config.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # configure shairpprt-sync on pi for multi instance support
-sp_installed=$(sudo apt list --installed 2> /dev/null | grep xauth -c)
+sp_installed=$(sudo apt list --installed 2> /dev/null | grep shairport-sync -c)
 if [ 0 -eq "${sp_installed}" ]; then
   echo "installing shairport-sync"
   sudo apt update && sudo apt install -y shairport-sync
