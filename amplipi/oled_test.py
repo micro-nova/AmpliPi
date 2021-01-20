65da0254d8e3072805e5d49147cb1f5a8e51c618
Mark Bowers <mark@micro-nova.com>
2021-01-20 11:45:44 -0500

    Add AmpliPi OLED driver

---
+++ amplipi/oled_test.py
@@ -0,0 +1,23 @@
+from oled import AmpliPi_OLED
+import time
+
+oled = AmpliPi_OLED()
+
+i = 0
+
+# demo 
+while(True):
+  # increment
+  i = i + 2
+  # rollover
+  if(i >= 100):
+    i = 0
+  # set volume bars
+  oled.set_volumes([i,i,i,i,i,i])
+  # sleep
+  time.sleep(1)
+  
+  
+  
+  
+  
