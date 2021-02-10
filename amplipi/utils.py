74c6baecadd03a456ab95b3e842bc85c50f3bb17
Lincoln Lorenz <lincoln@micro-nova.com>
2021-02-10 15:43:47 -0500

    Rename list

--- amplipi/utils.py
+++ amplipi/utils.py
@@ -50,8 +50,8 @@ def updated_val(update, val):
   else:
     return update, update != val
 
-def find(_list, id):
-  return next(filter(lambda ie: ie[1]['id'] == id, enumerate(_list)), (None, None))
+def find(items, id):
+  return next(filter(lambda ie: ie[1]['id'] == id, enumerate(items)), (None, None))
 
 def clamp(x, xmin, xmax):
     return max(xmin, min(x, xmax))
