cfd0fc952dea08b785427a7107267197daf4e808
Jeremy <jeremy@micro-nova.com>
2021-02-11 18:01:44 -0500

    Updates to all scripts involved in Shairport Metadata
    -streams.py: Pulls new data points out of currentSong, checks whether the picture is empty or not, then forwards the picture directory/name
    -shairport_metadata.bash: cleans out photo repositories from previous instances, runs the metadata reader from the image directory so picture data is properly saved in the right place
    sp_meta.py: Checks for the name and size of the image - this info gets saved in currentSong
    script.js: Now makes use of the shairport cover art

--- config/shairport_metadata.bash
+++ config/shairport_metadata.bash
@@ -1,3 +1,8 @@
 #!/bin/bash
+# Clear out any previous album cover images, then navigate to the proper directory #
+rm -r -f /home/pi/web/static/imgs/srcs/$1/
+mkdir -p /home/pi/web/static/imgs/srcs/$1/
+cd /home/pi/web/static/imgs/srcs/$1/
+
 # Start the metadata service with argument $1 being the source number #
 cat /home/pi/config/srcs/$1/shairport-sync-metadata | shairport-sync-metadata-reader | /home/pi/scripts/sp_meta.py $1
