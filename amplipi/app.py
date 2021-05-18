c517856e7d2b227d9e513d3c26241e4b45e64aa7
Lincoln Lorenz <lincoln@micro-nova.com>
2021-05-18 12:59:31 -0400

    Add route description and TODOs for next steps

--- amplipi/app.py
+++ amplipi/app.py
@@ -126,6 +126,7 @@ class API:
 
   @api_router.get('/', tags=['status'])
   def get_status(self) -> models.Status:
+    """ Get the system status and configuration """
     return self.ctrl.get_state()
 
   def code_response(self, resp):
@@ -142,26 +143,31 @@ class API:
 
   @api_router.get('/sources', tags=['source'])
   def get_sources(self) -> Dict[str, List[models.Source]]:
+    """ Get all sources """
     return {'sources' : self.ctrl.get_state().sources}
 
   @api_router.get('/sources/{sid}', tags=['source'])
   def get_source(self, sid: int) -> models.Source:
+    """ Get Source with id=**sid** """
     # TODO: add get_X capabilities to underlying API?
     sources = self.ctrl.get_state().sources
     return sources[sid]
 
   @api_router.patch('/sources/{sid}', tags=['source'])
   def set_source(self, sid: int, update: models.SourceUpdate) -> models.Status:
+    """ Update a source's configuration (source=**sid**) """
     return self.code_response(self.ctrl.set_source(sid, update))
 
   # zones
 
   @api_router.get('/zones', tags=['zone'])
   def get_zones(self) -> Dict[str, List[models.Zone]]:
+    """ Get all zones """
     return {'zones': self.ctrl.get_state().zones}
 
   @api_router.get('/zones/{zid}', tags=['zone'])
   def get_zone(self, zid: int) -> models.Zone:
+    """ Get Zone with id=**zid** """
     zones = self.ctrl.get_state().zones
     if zid >= 0 and zid < len(zones):
       return zones[zid]
@@ -169,21 +175,28 @@ class API:
       raise HTTPException(404, f'zone {zid} not found')
 
   @api_router.patch('/zones/{zid}', tags=['zone'])
-  def set_zone(self, zid: int, zone: models.ZoneUpdate):
+  def set_zone(self, zid: int, zone: models.ZoneUpdate) -> models.Status:
+    """ Update a zone's configuration (zone=**zid**) """
     return self.code_response(self.ctrl.set_zone(zid, zone))
+
+  # TODO: add set_zones(self, zones:List[int]=[], groups:List[int]=[], update:ZoneUpdate)
 
   # groups
 
   @api_router.post('/group', tags=['group'])
   def create_group(self, group: models.Group) -> models.Group:
+    """ Create a new grouping of zones """
+    # TODO: add named example group
     return self.code_response(self.ctrl.create_group(group))
 
   @api_router.get('/groups', tags=['group'])
   def get_groups(self) -> Dict[str, List[models.Group]]:
+    """ Get all groups """
     return {'groups' : self.ctrl.get_state().groups}
 
   @api_router.get('/groups/{gid}', tags=['group'])
   def get_group(self, gid: int) -> models.Group:
+    """ Get Group with id=**gid** """
     _, grp = utils.find(self.ctrl.get_state().groups, gid)
     if grp is not None:
       return grp
@@ -192,24 +205,30 @@ class API:
 
   @api_router.patch('/groups/{gid}', tags=['group'])
   def set_group(self, gid: int, group: models.GroupUpdate) -> models.Status:
+    """ Update a groups's configuration (group=**gid**) """
     return self.code_response(self.ctrl.set_group(gid, group)) # TODO: pass update directly
 
   @api_router.delete('/groups/{gid}', tags=['group'])
   def delete_group(self, gid: int) -> models.Status:
+    """ Delete a group (group=**gid**) """
     return self.code_response(self.ctrl.delete_group(id=gid))
 
   # streams
 
   @api_router.post('/stream', tags=['stream'])
   def create_stream(self, stream: models.Stream) -> models.Stream:
+    """ Create a new audio stream """
+    # TODO: add an example stream for each type of stream
     return self.code_response(self.ctrl.create_stream(stream))
 
   @api_router.get('/streams', tags=['stream'])
   def get_streams(self) -> Dict[str, List[models.Stream]]:
+    """ Get all streams """
     return {'streams' : self.ctrl.get_state().streams}
 
   @api_router.get('/streams/{sid}', tags=['stream'])
   def get_stream(self, sid: int) -> models.Stream:
+    """ Get Stream with id=**sid** """
     _, stream = utils.find(self.ctrl.get_state().streams, sid)
     if stream is not None:
       return stream
@@ -218,28 +237,37 @@ class API:
 
   @api_router.patch('/streams/{sid}', tags=['stream'])
   def set_stream(self, sid: int, update: models.StreamUpdate) -> models.Status:
+    """ Update a stream's configuration (stream=**sid**) """
     return self.code_response(self.ctrl.set_stream(sid, update))
 
   @api_router.delete('/streams/{sid}', tags=['stream'])
   def delete_stream(self, sid: int) -> models.Status:
+    """ Delete a stream """
     return self.code_response(self.ctrl.delete_stream(sid))
 
   @api_router.post('/streams/{sid}/{cmd}', tags=['stream'])
   def exec_command(self, sid: int, cmd: str) -> models.Status:
+    """ Execute a comamnds on a stream (stream=**sid**).
+
+    Currently only available with Pandora streams"""
     return self.code_response(self.ctrl.exec_stream_command(sid, cmd=cmd))
 
   # presets
 
   @api_router.post('/preset', tags=['preset'])
   def create_preset(self, preset: models.Preset) -> models.Preset:
+    """ Create a new preset configuration """
+    # TODO: add several example presets
     return self.code_response(self.ctrl.create_preset(preset))
 
   @api_router.get('/presets', tags=['preset'])
   def get_presets(self) -> Dict[str, List[models.Preset]]:
+    """ Get all presets """
     return {'presets' : self.ctrl.get_state().presets}
 
   @api_router.get('/presets/{pid}', tags=['preset'])
   def get_preset(self, pid: int) -> models.Preset:
+    """ Get Preset with id=**pid** """
     _, preset = utils.find(self.ctrl.get_state().presets, pid)
     if preset is not None:
       return preset
@@ -248,14 +276,17 @@ class API:
 
   @api_router.patch('/presets/{pid}', tags=['preset'])
   async def set_preset(self, pid: int, update: models.PresetUpdate) -> models.Status:
+    """ Update a preset's configuration (preset=**pid**) """
     return self.code_response(self.ctrl.set_preset(pid, update))
 
   @api_router.delete('/presets/{pid}', tags=['preset'])
   def delete_preset(self, pid: int) -> models.Status:
+    """ Delete a preset """
     return self.code_response(self.ctrl.delete_preset(pid))
 
   @api_router.post('/presets/{pid}/load', tags=['preset'])
   def load_preset(self, pid: int) -> models.Status:
+    """ Load a preset configuration """
     return self.code_response(self.ctrl.load_preset(pid))
 
   # Documentation
@@ -270,10 +301,11 @@ app.include_router(api_router, prefix='/api')
 # add the root of the API as well, since empty paths are invalid this needs to be handled outside of the router
 @app.get('/api', response_model_exclude_none=True, tags=['status'])
 def get_status(ctrl: Api = Depends(get_ctrl)) -> models.Status:
+  """ Get the system status and configuration """
   return ctrl.get_state()
 
 # API Documentation
-def generate_openapi_spec():
+def generate_openapi_spec(add_test_docs=True):
   if app.openapi_schema:
     return app.openapi_schema
   openapi_schema = get_openapi(
@@ -281,14 +313,14 @@ def generate_openapi_spec():
     version = '1.0',
     description = """This is the AmpliPi home audio system's control server.
 
-      # Configuration
+    # Configuration
 
-      This web interface allows you to control and configure your AmpliPi device.
-      At the moment the API is the only way to configure the AmpliPi.
+    This web interface allows you to control and configure your AmpliPi device.
+    At the moment the API is the only way to configure the AmpliPi.
 
-      # OpenAPI
+    # OpenAPI
 
-      This API is documented using the OpenAPI specification
+    This API is documented using the OpenAPI specification
     """,
     routes = app.routes,
     tags = [
@@ -322,6 +354,47 @@ def generate_openapi_spec():
       'description': 'AmpliPi Controller'
     }],
   )
+  if add_test_docs:
+    openapi_schema['info']['description'] += """
+    ## Try it out!
+
+    __Using this web interface to test API commands:__
+
+      1. Go to an API request
+      1. Pick one of the examples
+      2. Edit it
+      3. Press try button, it will send an API command/request to the AmpliPi
+
+    __Try using the get status:__
+
+      1. Go to [Status -> Get Status](#get-/)
+      2. Click the Try button, you will see a response below with the full status/config of the AmpliPi controller
+
+    __Try creating a new group:__
+
+      1. Go to [Group -> Create Group](#post-/group)
+      2. Click Example
+      3. Edit the zones and group name
+      4. Click the try button, you will see a response with the newly created group
+
+    __Here are some other things that you might want to change:__
+
+      - [Stream -> Create new stream](#post-/stream)
+      - [Zone -> Update Zone](#patch-/zones/-zoneId-) (to change the zone name)
+      - [Preset -> Create preset](#post-/preset) (Have a look at the model to see what can be added here)
+
+    # More Info
+
+    Check out all of the different things you can do with this API:
+
+      - [Status](#tag--status)
+      - [Source](#tag--source)
+      - [Zone](#tag--zone)
+      - [Group](#tag--group)
+      - [Stream](#tag--stream)
+      - [Preset](#tag--preset)
+      """
+
   openapi_schema['contact'] = {
     'email': 'info@micro-nova.com',
     'name':  'Micronova',
@@ -331,6 +404,12 @@ def generate_openapi_spec():
     'name': 'GPL',
     'url':  '/license',
   },
+
+  # TODO: add relevant source ids
+  # TODO: add relevant zone ids
+  # TODO: add relevant group ids
+  # TODO: add relevant stream ids
+  # TODO: add relevant preset ids
   return openapi_schema
 
 app.openapi = generate_openapi_spec
