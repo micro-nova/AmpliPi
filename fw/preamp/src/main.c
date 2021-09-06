1dce7b6ad34c8163c7c1f5fe8c42a587e37e4074
Michael Lohrer <michael@micro-nova.com>
2021-09-06 17:53:23 -0400

    I2C clock calc for standard mode

--- fw/preamp/src/main.c
+++ fw/preamp/src/main.c
@@ -99,6 +99,16 @@ void init_gpio() {
 
 void init_i2c1(uint8_t preamp_addr) {
   // I2C1 is from control board
+  //
+  // Single AmpliPi unit:
+  //  t_r = ~370 ns
+  //  t_f = ~5.3 ns
+  // Single expansion unit:
+  //  t_r = ~450 ns
+  //  t_f = ~7.2 ns
+  // Two expansion units:
+  //  t_r = ~600 ns
+  //  t_f = ~9.4 ns
 
   // Enable peripheral clock for I2C1
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
@@ -124,14 +134,44 @@ void init_i2c1(uint8_t preamp_addr) {
   I2C_InitStructure1.I2C_OwnAddress1         = preamp_addr;
   I2C_InitStructure1.I2C_Ack                 = I2C_Ack_Enable;
   I2C_InitStructure1.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-  // I2C_InitStructure1.I2C_Timing = 0x10805E89; // TODO: Determine and document
-  // why this value is needed for I2C2 but not I2C1
+  I2C_InitStructure1.I2C_Timing = 0;  // Clocks not generated in slave mode
   I2C_Init(I2C1, &I2C_InitStructure1);
   I2C_Cmd(I2C1, ENABLE);
 }
 
 void init_i2c2() {
-  // I2C2 is to volume chips, power board, and front panel
+  /* I2C-2 is internal to a single AmpliPi unit.
+   * The STM32 is the master and controls the volume chips, power, fans,
+   * and front panel LEDs.
+   *
+   * Bus Capacitance
+   * | Device           | Capacitance (pF)
+   * | STM32            | 5
+   * | MAX11601 (ADC)   | 15 (t_HD.STA>.6 t_LOW>1.3 t_HIGH>0.6 t_SU.STA>.6
+   *                          t_HD.DAT<.15? t_SU.DAT>0.1 t_r<.3 t_f<.3)
+   * | MCP23008 (Power) | ?? (t_HD.STA>.6 t_LOW>1.3 t_HIGH>0.6 t_SU.STA>.6
+   *                          t_HD.DAT<.9   t_SU.DAT>0.1 t_r<.3 t_f<.3)
+   * | MCP23008 (LEDs)  | ??
+   * | MCP4017 (DPot)   | 10 (t_HD.STA>.6 t_LOW>1.3 t_HIGH>0.6 t_SU.STA>.6
+   *                          t_HD.DAT<.9   t_SU.DAT>0.1 t_r<.3 t_f<.04)
+   * | TDA7448 (Vol1)   | ??????????????
+   * | TDA7448 (Vol2)   | Doesn't even specify max frequency...
+   * ~70 pF for all devices, plus say ~20 pF for all traces and wires = ~90 pF
+   * So rise time t_r ~= 0.8473 * 1 kOhm * 90 pF = 76 ns
+   * Measured rise time: 72 ns
+   * Measured fall time:  4 ns
+   *
+   * Pullup Resistor Values
+   *   Max output current for I2C Standard/Fast mode is 3 mA, so min pullup is:
+   *    Rp > [V_DD - V_OL(max)] / I_OL = (3.3 V - 0.4 V) / 3 mA = 967 Ohm
+   *   Max bus capacitance (with only resistor for pullup) is 200 pF.
+   *   Standard mode rise-time t_r(max) = 1000 ns
+   *    Rp_std < t_r(max) / (0.8473 * C_b) = 1000 / (0.8473 * 0.2) = 5901 Ohm
+   *   Fast mode rise-time t_r(max) = 300 ns
+   *    Rp_fast < t_r(max) / (0.8473 * C_b) = 1000 / (0.8473 * 0.2) = 1770 Ohm
+   *   For Standard mode: 1k <= Rp <= 5.6k
+   *   For Fast mode: 1k <= Rp <= 1.6k
+   */
 
   // Enable peripheral clock for I2C2
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C2, ENABLE);
@@ -161,8 +201,66 @@ void init_i2c2() {
   I2C_InitStructure2.I2C_OwnAddress1         = 0x00;
   I2C_InitStructure2.I2C_Ack                 = I2C_Ack_Enable;
   I2C_InitStructure2.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-  // From 48-MHz PCLK with 100ns rise, 10ns fall -> 100 KHz
-  I2C_InitStructure2.I2C_Timing = 0x10805E89;
+
+  // Datasheet example: 100 kHz: 0x10420F13, 400 kHz: 0x10310309
+  // Excel tool, rise/fall 76/15 ns: 100 kHz: 0x00201D2B (0.5935% error)
+  //                                 400 kHz: 0x0010020A (2.4170% error)
+
+  // Common parameters
+  // t_I2CCLK = 1 / 8 MHz = 125 ns
+  // t_AF(min) = 50 ns
+  // t_AF(max) = 260 ns
+  // t_r = 72 ns
+  // t_f = 4 ns
+  // Fall time must be < 300 ns
+  // For Standard mode (100 kHz), rise time < 1000 ns
+  // For Fast mode (400 kHz), rise time < 300 ns
+  // tR = 0.8473*Rp*Cb = 847.3*Cb
+
+  // Standard mode, max 100 kHz
+  // t_LOW > 4.7 us
+  // t_HIGH > 4 us
+  // t_I2CCLK < [t_LOW - t_AF(min) - t_DNF] / 4 = (4700 - 50) / 4 = 1.1625 ns
+  // t_I2CCLK < t_HIGH = 4000 ns
+  // Set PRESC = 0, so t_I2CCLK = 1 / 8 MHz = 125 ns
+  // t_PRESC = t_I2CCLK / (PRESC + 1) = 125 / (0 + 1) = 125 ns
+  // SDADEL >= [t_f + t_HD;DAT(min) - t_AF(min) - t_DNF - 3*t_I2CCLK] / t_PRESC
+  // SDADEL >= [t_f - 50 - 375] / 125 --- This will be < 0, so SDASEL >= 0
+  // SDADEL <= [t_HD;DAT(max) - t_r - t_AF(max) - t_DNF - 4*t_I2CCLK] / t_PRESC
+  // SDADEL <= (3450 - 76 - 260 - 500) / 125 = 20.912
+  // SCLDEL >= {[t_r + t_SU;DAT(min)] / t_PRESC} - 1
+  // SCLDEL >= (76 + 250) / 125 - 1 = 1.608
+  // So 0 <= SDADEL <= 20, SCLDEL >= 2
+  // I2C_TIMINGR[31:16] = 0x0020
+  //
+  // t_HIGH(min) <= t_AF(min) + t_DNF + 2*t_I2CCLK + t_PRESC*(SCLH + 1)
+  // 4000 <= 50 + 2*125 + 125*(SCLH + 1)
+  // 3575 <= 125*SCLH
+  // SCLH >= 28.6 = 0x1D
+  //
+  // t_LOW(min) <= t_AF(min) + t_DNF + 2*t_I2CCLK + t_PRESC*(SCLL + 1)
+  // 4700 <= 50 + 2*125 + 125*(SCLL + 1)
+  // 4275 <= 125*SCLL
+  // SCLL >= 34.2 = 0x23
+  //
+  // Need to stay under 100 kHz in "worst" case. Keep SCLH at min,
+  // but here we determine final SCLL.
+  // t_SCL = t_SYNC1 + t_SYNC2 + t_LOW + t_HIGH >= 10000 ns (100 kHz max)
+  // t_SYNC1(min) = t_f + t_AF(min) + t_DNF + 2*t_I2CCLK
+  // t_SYNC1(min) = 6 + 50 + 2*125 = 306 ns
+  // t_SYNC2(min) = t_r + t_AF(min) + t_DNF + 2*t_I2CCLK
+  // t_SYNC2(min) = 76 + 50 + 2*125 = 376 ns
+  // t_SYNC1 + t_SYNC2 + t_LOW + t_HIGH >= 10000 ns
+  // t_LOW + t_HIGH >= 9318 ns
+  // t_PRESC*(SCLL + 1) + t_PRESC*(SCLH + 1) >= 9318 ns
+  // 125*(SCLL + 1) + 125*30 >= 9318 ns
+  // 125*(SCLL + 1) + 125*30 >= 9318 ns
+  // SCLL >= 43.544 = 0x2C
+
+  // Fast mode, max 400 kHz
+  // TODO?
+
+  I2C_InitStructure2.I2C_Timing = 0x00201D2C;
   I2C_Init(I2C2, &I2C_InitStructure2);
   I2C_Cmd(I2C2, ENABLE);
 }
