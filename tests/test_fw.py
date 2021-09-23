d242da74ecf4516c34aa4de5d4d0ea916a47966e
Michael Lohrer <michael@micro-nova.com>
2021-09-22 23:05:27 -0400

    Add firmware building and flashing tests

--- tests/test_fw.py
+++ tests/test_fw.py
@@ -20,42 +20,62 @@ NUM_EXPANDERS = 0
 # Preamp firmware .bin file path, relative to amplipi's directory.
 BIN_FILE = 'fw/preamp/build/preamp_bd.bin'
 
-def build_preamp_fw(amplipi_dir) -> str:
+@pytest.fixture
+def fw_path(request) -> str:
+  return f'{request.fspath.dirname}/../fw/preamp'
+
+@pytest.fixture
+def fw_bin_path(fw_path) -> str:
+  return f'{fw_path}/build/preamp_bd.bin'
+
+@pytest.mark.dependency()
+def test_fw_build(fw_path):
   """ Build the firmware from source """
-  # TODO: Support debug build and setting version
-  #mkdir = subprocess.run(['mkdir fw/preamp/build'], check=True)
-  #if mkdir.returncode != 0:
-  #  return False
-  #flash = subprocess.run([f'fw/'])
+  # TODO: Build in a temp directory
 
-  exists = subprocess.run([f'ls {amplipi_dir}/{BIN_FILE}'], shell=True, check=False)
-  if exists.returncode == 0:
-    return BIN_FILE
-  return None
+  # Configure with CMake
+  config_result = subprocess.run([f'cmake -B {fw_path}/build'], cwd=fw_path,
+                                 shell=True, check=False)
+  assert config_result.returncode == 0
 
-# TODO: test firmware building
+  # Clean an previous build results
+  clean_result = subprocess.run([f'cmake --build {fw_path}/build --target clean'],
+                                cwd=fw_path, shell=True, check=False)
+  assert clean_result.returncode == 0
+
+  # Build the firmware
+  build_result = subprocess.run([f'cmake --build {fw_path}/build -j4'],
+                                cwd=fw_path, shell=True, check=False)
+  assert build_result.returncode == 0
 
 # Only run the following tests on AmpliPi
 if not amplipi.utils.is_amplipi():
   pytest.skip("Not running on AmpliPi hardware, skipping firmware tests", allow_module_level = True)
 
-@pytest.mark.dependency()
-def test_flash_master(request):
-  amplipi_dir = f'{request.fspath.dirname}/..'
-  bin_path = build_preamp_fw(amplipi_dir)
-  assert bin_path is not None
-
+@pytest.mark.dependency(depends=['test_fw_build'])
+def test_flash_master(fw_bin_path):
   preamps = amplipi.hw.Preamps()
-  preamps.flash(bin_path)
+  preamps.flash(fw_bin_path, num_units=1)
   ver = preamps[0].read_version()
   print(ver)
   assert ver[0] == 1
 
-
 @pytest.mark.dependency(depends=['test_flash_master'])
-def test_read_i2c():
+def test_set_address():
   preamps = amplipi.hw.Preamps()
-  assert preamps[NUM_EXPANDERS] is not None
+  for i in range(10):
+    preamps._reset_master(bootloader = False)
+    if not preamps.send_i2c_address():
+      pytest.fail(f'Failed resetting then setting address after {i} tries.')
+  for i in range(10):
+    if not preamps.send_i2c_address():
+      pytest.fail(f'Failed setting address after {i} tries.')
+    #ver = preamps[0].read_version()
+
+# @pytest.mark.dependency(depends=['test_flash_master'])
+# def test_read_i2c():
+#   preamps = amplipi.hw.Preamps()
+#   assert preamps[NUM_EXPANDERS] is not None
 
 #@pytest.mark.dependency(depends=['test_read_i2c'])
 #def test_multi_baud():
