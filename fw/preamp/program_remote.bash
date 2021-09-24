0810b202c542536a5ed715d9bce8c4d02a2e3647
Michael Lohrer <michael@micro-nova.com>
2021-09-24 19:39:10 -0400

    Remote programming script only programs

--- fw/preamp/program_remote.bash
+++ fw/preamp/program_remote.bash
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 # This script assumes a directory 'amplipi-test' exists on the remote AmpliPi
 # that has the prerequisites to flashing the preamp already setup.
-remote_host=${1:-amplipi105.local}
-remote_dir=${2:-amplipi-test}
+fw_bin=${1:-preamp_bd.bin}
+remote_host=${2:-amplipi105.local}
+remote_dir=${3:-amplipi-test}
 
-echo "Building firmware locally then programming on $remote_host using ~/$remote_dir"
-cd "$(dirname "${BASH_SOURCE[0]}")"
-mkdir -p build
-cd build
-cmake ..
-make -j
-scp preamp_bd.bin pi@$remote_host:$remote_dir
-ssh pi@$remote_host -- "cd amplipi-test; venv/bin/python -m amplipi.hw --flash preamp_bd.bin"
+echo "Programming $fw_bin on $remote_host using ~/$remote_dir"
+#echo "Building firmware locally then programming on $remote_host using ~/$remote_dir"
+#cd "$(dirname "${BASH_SOURCE[0]}")"
+#mkdir -p build
+#cd build
+#cmake ..
+#make -j
+scp $fw_bin pi@$remote_host:$remote_dir/
+ssh pi@$remote_host -- "cd $remote_dir; venv/bin/python -m amplipi.hw --flash preamp_bd.bin"
