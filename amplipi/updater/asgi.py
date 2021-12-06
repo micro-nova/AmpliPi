c9fab736234fe12e104a70c4bc9cb75c748e4653
Lincoln Lorenz <lincoln@micro-nova.com>
2021-12-06 17:38:13 -0500

    Cleanup updater backend

--- amplipi/updater/asgi.py
+++ amplipi/updater/asgi.py
@@ -20,14 +20,6 @@
 
 Simple web based software updates
 """
-
-# web framework
-from fastapi import FastAPI, Request, File, UploadFile
-from sse_starlette.sse import EventSourceResponse
-from fastapi.staticfiles import StaticFiles
-from starlette.responses import FileResponse
-#web server
-import uvicorn
 # file and process handling
 import os
 import subprocess
@@ -35,7 +27,6 @@ import glob
 import sys
 from tempfile import mkdtemp
 import re
-import requests
 import json
 import threading
 import time
@@ -43,8 +34,15 @@ import queue
 import pathlib
 import shutil
 import asyncio
-import pkg_resources
-from typing import List
+# web framework
+import requests
+from fastapi import FastAPI, Request, File, UploadFile
+from fastapi.staticfiles import StaticFiles
+from sse_starlette.sse import EventSourceResponse
+from starlette.responses import FileResponse
+# web server
+import uvicorn
+# models
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -56,19 +54,20 @@ class ReleaseInfo(BaseModel):
   version: str
 
 # host all of the static files the client will look for
-# TODO: put static files somewhere else?
 real_path = os.path.realpath(__file__)
 dir_path = os.path.dirname(real_path)
 app.mount("/static", StaticFiles(directory=f"{dir_path}/static"), name="static")
 
-home = f"{os.environ.get('HOME')}/amplipi-dev" # standard install directory
+HOME = f"{os.environ.get('HOME')}/amplipi-dev" # standard install directory
 
 @app.get('/update')
 def get_index():
-  print('get index!')
-  return FileResponse(f'{dir_path}/static/index.html') # FileResponse knows nothing about the static mount
+  """ Get the update website """
+  # FileResponse knows nothing about the static mount
+  return FileResponse(f'{dir_path}/static/index.html')
 
 def save_upload_file(upload_file: UploadFile, destination: pathlib.Path) -> None:
+  """ Save the update file """
   try:
     with destination.open("wb") as buffer:
       shutil.copyfileobj(upload_file.file, buffer)
@@ -76,12 +75,14 @@ def save_upload_file(upload_file: UploadFile, destination: pathlib.Path) -> None
     upload_file.file.close()
 
 @app.post("/update/upload")
-async def start_update(file: UploadFile = File(...)):
+async def start_upload(file: UploadFile = File(...)):
+  """ Start a upload based update """
   print(file.filename)
   try:
     # TODO: use a temp directory and pass it the installation
     os.makedirs('web/uploads', exist_ok=True)
     save_upload_file(file, pathlib.Path('web/uploads/update.tar.gz'))
+    # TODO: verify file has amplipi version
     return 200
   except Exception as e:
     print(e)
@@ -90,10 +91,11 @@ async def start_update(file: UploadFile = File(...)):
 def download(url, file_name):
   """ Download a binary file from @url to @file_name """
   with open(file_name, "wb") as file:
-      # get request
-      response = requests.get(url)
-      # write to file
-      file.write(response.content)
+    # get request
+    response = requests.get(url)
+    # write to file
+    file.write(response.content)
+    # TODO: verify file has amplipi version
 
 @app.post("/update/download")
 async def download_update(info: ReleaseInfo ):
@@ -109,17 +111,21 @@ async def download_update(info: ReleaseInfo ):
 
 @app.get('/update/restart')
 def restart():
-  subprocess.Popen(f'python3 {home}/scripts/configure.py --restart-updater'.split())
-  return {}
+  """ Restart the update service
 
+  This is typically done at the end of an update
+  """
+  subprocess.Popen(f'python3 {HOME}/scripts/configure.py --restart-updater'.split())
+  return {}
 
 TOML_VERSION_STR = re.compile(r'version\s*=\s*"(.*)"')
 
 @app.get('/update/version')
-def version():
+def get_version():
   """ Get the AmpliPi software version from the project TOML file """
-  # assume the application is running in its base directory and check the pyproject.toml file to determine the version
-  # this is needed for a straight github checkout (the common developement paradigm at MicroNova)
+  # Assume the application is running in its base directory and check the pyproject.toml file
+  # to determine the version. This is needed for a straight github checkout
+  # (the common developement paradigm at MicroNova)
   version = 'unknown'
   updater_folder = os.path.dirname(os.path.realpath(__file__))
   try:
@@ -133,25 +139,28 @@ def version():
     pass
   return {'version': version}
 
-def sse_message(t, msg):
+def _sse_message(t, msg):
+  """ Report an SSE message """
   msg = msg.replace('\n', '<br>')
   sse_msg = {'data' : json.dumps({'message': msg, 'type' : t})}
   sse_messages.put(sse_msg)
-  time.sleep(0.1) # Give the SSE publisher time to handle the messages, is there a way to just yield?
+  # Give the SSE publisher time to handle the messages, is there a way to just yield?
+  time.sleep(0.1)
 
-def sse_info(msg):
-  sse_message('info', msg)
-def sse_warning(msg):
-  sse_message('warning', msg)
-def sse_error(msg):
-  sse_message('error', msg)
-def sse_done(msg):
-  sse_message('success', msg)
-def sse_failed(msg):
-  sse_message('failed', msg)
+def _sse_info(msg):
+  _sse_message('info', msg)
+def _sse_warning(msg):
+  _sse_message('warning', msg)
+def _sse_error(msg):
+  _sse_message('error', msg)
+def _sse_done(msg):
+  _sse_message('success', msg)
+def _sse_failed(msg):
+  _sse_message('failed', msg)
 
 @app.route('/update/install/progress')
 async def progress(req: Request):
+  """ SSE Progress server """
   async def stream():
     try:
       while True:
@@ -172,13 +181,14 @@ async def progress(req: Request):
 def extract_to_home(home):
   """ The simple, pip-less install. Extract tarball and copy into users home directory """
   temp_dir = mkdtemp()
-  sse_info(f'Extracting software to temp directory {temp_dir}')
+  _sse_info(f'Extracting software to temp directory {temp_dir}')
   file_list = subprocess.getoutput('tar -tvf web/uploads/update.tar.gz')
   # get the full name of the release
   release = re.search(r'(amplipi-.*?)/', file_list, flags=re.IGNORECASE).group(1)
-  sse_info(f'Got amplipi release: {release}')
-  out = subprocess.run('tar -xf web/uploads/update.tar.gz --directory={}'.format(temp_dir).split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-  sse_info('copying software')
+  _sse_info(f'Got amplipi release: {release}')
+  subprocess.run('tar -xf web/uploads/update.tar.gz --directory={}'.format(temp_dir).split(),
+                 stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True)
+  _sse_info('copying software')
   files_to_copy = ' '.join(glob.glob(f'{temp_dir}/{release}/*'))
   subprocess.check_call(f'mkdir -p {home}'.split())
   subprocess.check_call(f'cp -a {files_to_copy}  {home}/'.split())
@@ -190,41 +200,43 @@ def indent(p: str):
 def install_thread():
   """ Basic tar.gz based installation """
 
-  sse_info('starting installation')
+  _sse_info('starting installation')
 
   try:
-    extract_to_home(home)
-    sse_info('done copying software')
+    extract_to_home(HOME)
+    _sse_info('done copying software')
   except Exception as e:
-    sse_failed(f'installation failed, error extracting release: {e}')
+    _sse_failed(f'installation failed, error extracting release: {e}')
     return
 
   try:
     # use the configure script provided by the new install to configure the installation
-    sys.path.insert(0, f'{home}/scripts')
+    sys.path.insert(0, f'{HOME}/scripts')
     import configure # we want the new configure! # pylint: disable=import-error,import-outside-toplevel
-    def progress_sse(tl):
-      for task in tl:
-        sse_info(task.name)
+    def progress_sse(tasks):
+      for task in tasks:
+        _sse_info(task.name)
         output = indent(task.output)
         if task.success:
           print(f'info: {output}')
-          sse_info(output)
+          _sse_info(output)
         else:
           print(f'error: {output}')
-          sse_error(output)
-    # reconfigure and restart everything but the updater (which is restarted later by update/restart)
+          _sse_error(output)
+    # reconfigure and restart everything but the updater
+    # (which is restarted later by update/restart)
     success = configure.install(progress=progress_sse)
     if success:
-      sse_done('installation done')
+      _sse_done('installation done')
     else:
-      sse_failed('installation failed')
+      _sse_failed('installation failed')
   except Exception as e:
-    sse_failed(f'installation failed, error configuring update: {e}')
+    _sse_failed(f'installation failed, error configuring update: {e}')
     return
 
 @app.get('/update/install')
 def install():
+  """ Start the install after update is downloaded """
   t = threading.Thread(target=install_thread)
   t.start()
   return {}
