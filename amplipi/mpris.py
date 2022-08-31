1dfa938d8a2c426cbddb98e3a1a138b300518819
Klayton Smith II <klayton@micro-nova.com>
2022-08-31 12:45:51 -0400

    wip

--- amplipi/mpris.py
+++ amplipi/mpris.py
@@ -2,10 +2,14 @@
 
 from dataclasses import dataclass
 from enum import Enum, auto
+import json
+import time
 from tokenize import String
 from typing import List
 from dasbus.connection import SessionMessageBus
-from dasbus.identifier import DBusServiceIdentifier
+from multiprocessing import Process
+
+import amplipi.utils as utils
 
 
 METADATA_MAPPINGS = [
@@ -14,6 +18,9 @@ METADATA_MAPPINGS = [
   ('art_url', 'mpris:artUrl'),
   ('album', 'xesam:album')
 ]
+
+METADATA_REFRESH_RATE = 0.5
+METADATA_FILE_NAME = "metadata.txt"
 
 class CommandTypes(Enum):
   PLAY = auto()
@@ -28,17 +35,35 @@ class Metadata:
   title: String = ''
   art_url: String = ''
   album: String = ''
+  state: String = ''
 
 
 class MPRIS:
   """A class for interfacing with an MPRIS MediaPlayer2 over dbus."""
 
-  def __init__(self, service_suffix) -> None:
+  def __init__(self, service_suffix, src) -> None:
     self.mpris = SessionMessageBus().get_proxy(
         service_name = f"org.mpris.MediaPlayer2.{service_suffix}",
         object_path = "/org/mpris/MediaPlayer2",
         interface_name = "org.mpris.MediaPlayer2.Player"
     )
+
+    self.capabilities = []
+
+    self.service_suffix = service_suffix
+    self.src = src
+    self.metadata_path = f'{utils.get_folder("config")}/srcs/{self.src}/{METADATA_FILE_NAME}'
+
+    try:
+      with open(self.metadata_path, "w", encoding='utf-8') as f:
+        m = Metadata()
+        m.state = "Stopped"
+        json.dump(m.__dict__, f)
+    except Exception as e:
+      print (f'Exception clearing metadata file: {e}')
+
+    self.metadata_process = Process(target=self._metadata_reader)
+    self.metadata_process.start()
 
   def play(self) -> None:
     """Plays."""
@@ -60,41 +85,87 @@ class MPRIS:
     """Plays or pauses depending on current state."""
     self.mpris.PlayPause()
 
+  def _load_metadata(self):
+    try:
+      with open(self.metadata_path, 'r', encoding='utf-8') as f:
+        metadata_dict = json.load(f)
+        metadata_obj = Metadata()
+
+        for k in metadata_dict.keys():
+          metadata_obj.__dict__[k] = metadata_dict[k]
+
+        return metadata_obj
+    except Exception as e:
+      print(f"mpris loading metadata at {self.metadata_path} failed: {e}")
+
   def metadata(self) -> Metadata:
     """Returns metadata from MPRIS."""
-    raw_metadata = self.mpris.Metadata
-    metadata_obj = Metadata()
-
-    for mapping in METADATA_MAPPINGS:
-      try:
-        metadata_obj.__dict__[mapping[0]] = str(raw_metadata[mapping[1]]).strip("[]'")
-      except KeyError:
-        pass
-
-    return metadata_obj
+    return self._load_metadata()
 
   def is_playing(self) -> bool:
     """Playing?"""
-    return self.mpris.PlaybackStatus.strip("'") == 'Playing'
+    return self._load_metadata().state == 'Playing'
 
   def is_stopped(self) -> bool:
     """Stopped?"""
-    return self.mpris.PlaybackStatus.strip("'") == 'Stopped'
+    return self._load_metadata().state == 'Stopped'
 
   def get_capabilities(self) -> List[CommandTypes]:
     """Returns a list of supported commands."""
-    capabilities = []
 
-    if self.mpris.CanPlay:
-      capabilities.append(CommandTypes.PLAY)
+    if len(self.capabilities) == 0:
 
-    if self.mpris.CanPause:
-      capabilities.append(CommandTypes.PAUSE)
+      if self.mpris.CanPlay:
+        self.capabilities.append(CommandTypes.PLAY)
 
-    if self.mpris.CanGoNext:
-      capabilities.append(CommandTypes.NEXT)
+      if self.mpris.CanPause:
+        self.capabilities.append(CommandTypes.PAUSE)
 
-    if self.mpris.CanGoPrevious:
-      capabilities.append(CommandTypes.PREVIOUS)
+      if self.mpris.CanGoNext:
+        self.capabilities.append(CommandTypes.NEXT)
 
-    return capabilities
+      if self.mpris.CanGoPrevious:
+        self.capabilities.append(CommandTypes.PREVIOUS)
+
+    return self.capabilities
+
+  def __del__(self):
+    try:
+      self.metadata_process.terminate()
+    except:
+      pass
+
+  def _metadata_reader(self):
+    """Method run by the metadata process, also handles playing/paused."""
+
+    mpris = SessionMessageBus().get_proxy(
+      service_name = f"org.mpris.MediaPlayer2.{self.service_suffix}",
+      object_path = "/org/mpris/MediaPlayer2",
+      interface_name = "org.mpris.MediaPlayer2.Player"
+    )
+
+    m = Metadata()
+    m.state = 'Stopped'
+    last_sent = m.__dict__
+
+    while True:
+      try:
+        raw_metadata = mpris.Metadata
+        metadata = {}
+
+        for mapping in METADATA_MAPPINGS:
+          try:
+            metadata[mapping[0]] = str(raw_metadata[mapping[1]]).strip("[]'")
+          except KeyError:
+            pass
+
+        metadata['state'] = mpris.PlaybackStatus.strip("'")
+
+        if metadata != last_sent:
+          last_sent = metadata
+          with open(self.metadata_path, 'w', encoding='utf-8') as metadata_file:
+            json.dump(metadata, metadata_file)
+
+      except:
+        pass
+      time.sleep(1.0/METADATA_REFRESH_RATE)
