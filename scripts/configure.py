b9a3122ef3945bb391147b32c9d966c1c19a7fe2
Lincoln Lorenz <lincoln@micro-nova.com>
2023-02-01 15:00:07 -0500

    Dodge tmp directory collisions

--- scripts/configure.py
+++ scripts/configure.py
@@ -127,7 +127,7 @@ _os_deps: Dict[str, Dict[str, Any]] = {
               {'from': 'bin/ARCH/shairport-sync', 'to': 'streams/shairport-sync'}],
     'script': [
         'if which nqptp  > /dev/null; then exit 0; fi',
-        'pushd /tmp',
+        'pushd $(mktemp --directory)',
         'git clone https://github.com/mikebrady/nqptp.git',
         'pushd nqptp',
         'autoreconf -fi',
