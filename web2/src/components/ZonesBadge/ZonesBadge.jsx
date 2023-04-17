18978aad022f57042d025ea1dffc61ee642fefc1
Klayton Smith II <klayton@micro-nova.com>
2023-04-17 10:19:08 -0400

    made zones and streams on front page chips

--- web2/src/components/ZonesBadge/ZonesBadge.jsx
+++ web2/src/components/ZonesBadge/ZonesBadge.jsx
@@ -2,25 +2,42 @@ import './ZonesBadge.scss';
 import { useState, useEffect } from 'react';
 import { getSourceZones } from "@/pages/Home/Home.jsx"
 import { useStatusStore } from '@/App.jsx'
+import { chipClasses } from '@mui/material';
+
+const ZoneChip = ({ zone }) => {
+  return (
+    <div className='zone-chip'>
+      <div className='zone-text'>
+        {zone.name}
+      </div>
+    </div>
+  )
+}
 
 const ZonesBadge = ({ sourceId }) => {
   const allZones = useStatusStore((s) => s.status.zones)
   const usedZones = getSourceZones(sourceId, allZones)
 
-  let zones_text = ''
-  if(usedZones.length > 2) {
-    zones_text = `${usedZones[0].name} and ${usedZones.length - 1} more`
-  } else if(usedZones.length == 2) {
-    zones_text = `${usedZones[0].name} and ${usedZones[1].name}`
-  } else if(usedZones.length == 1) {
-    zones_text = `${usedZones[0].name}`
+
+  let chips = [];
+
+  switch (usedZones.length) {
+    case 0:
+      chips.push(<ZoneChip key={0} zone={{ name: 'Add Zones' }} />)
+      break
+    case 1:
+      chips.push(<ZoneChip key={0} zone={usedZones[0]} />)
+      break
+    case 2:
+      chips.push(<ZoneChip key={0} zone={usedZones[0]} />)
+      chips.push(<ZoneChip key={1} zone={usedZones[1]} />)
+      break
+    default:
+      chips.push(<ZoneChip key={0} zone={usedZones[0]} />)
+      chips.push(<ZoneChip key={1} zone={{ name: `+${usedZones.length - 2} more` }} />)
   }
 
-  return (
-    <div className='zone-text'>
-      {zones_text}
-    </div>
-  );
+  return <div>{chips}</div>
 }
 
-export default ZonesBadge;
+export default ZonesBadge
