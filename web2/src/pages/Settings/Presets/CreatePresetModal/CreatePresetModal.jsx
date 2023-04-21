f28e5f3e0312ca282b01f6cb10a1e6edd6db8c9a
Jonah Shader <jonah@micro-nova.com>
2023-04-21 17:59:21 -0400

    Tree wip

--- web2/src/pages/Settings/Presets/CreatePresetModal/CreatePresetModal.jsx
+++ web2/src/pages/Settings/Presets/CreatePresetModal/CreatePresetModal.jsx
@@ -1,31 +1,45 @@
 import { useState } from "react"
+import { useStatusStore } from "@/App"
 import Modal from '@/components/Modal/Modal'
 import Card from '@/components/Card/Card'
 import DoneIcon from '@mui/icons-material/Done'
 import './CreatePresetModal.scss'
 
-import List from '@mui/material/List';
-import ListSubheader from '@mui/material/ListSubheader';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
-import Collapse from '@mui/material/Collapse';
-import StarBorder from '@mui/icons-material/StarBorder';
-import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Box from '@mui/material/Box';
+
+const baseDict = (open, name, content) => {
+  return {
+    'open' : open,
+    'checked' : false,
+    'name' : name,
+    'content' : content
+  }
+}
+
+const dictWithOptions = (name) => {
+  return baseDict(false, name, [
+    baseDict(false, 'Stream', []),
+    baseDict(false, 'Volume', []),
+    baseDict(false, 'Zones', []),
+  ])
+}
+
+const buildTreeDict = (status) => {
+  const statusClone = JSON.parse(JSON.stringify(status))
+
+  const sourceDicts = statusClone.sources.filter(source => source.info.state !== 'stopped').map(source => dictWithOptions(source.info.name));
+
+  const top = baseDict(false, "All", sourceDicts)
+  return top;
+}
 
 const isEmpty = (dict) => Object.keys(dict).length === 0
 
 const DictToTree = ({dict, depth=0}) => {
   if (isEmpty(dict)) return <div />
-  console.log("DictToTree depth " + depth)
-
-  // return <div> HELLOOOOOOOOOO</div>
-
   let entries = []
   let i = 0
   for (const [key, value] of Object.entries(dict)) {
@@ -35,7 +49,7 @@ const DictToTree = ({dict, depth=0}) => {
         <Box key={i} sx={{ display: 'flex', flexDirection: 'column', ml: 3*depth }}>
           <FormControlLabel
           label={key}
-          control={<Checkbox />}
+          control={<Checkbox checked={0}/>}
           />
           <DictToTree dict={value} depth={depth+1} />
         </Box>
@@ -46,46 +60,105 @@ const DictToTree = ({dict, depth=0}) => {
   }
 
   return entries
+}
 
+const Structured2Child = ({checked, setChecked, level, setLevel, index}) => {
+  const myChecked = checked[index]
+  const myLevel = level[index]
+
+}
+
+const getTotalCheckboxes = (d) => d.content.reduce((partialSum, d2) => partialSum + getTotalCheckboxes(d2), 1)
+
+const Structured2Top = ({dict}) => {
+  // this function uses a flat representation of the tree structure.
+  // each child will get the state getters/setters along with their index within the state
+  const totalCheckboxes = getTotalCheckboxes(dict)
+  const [checked, setChecked] = useState(Array(totalCheckboxes).fill('unchecked'))
+  const [level, setLevel] = useState(Array(totalCheckboxes).fill(0))
+}
+
+const StructuredDictAsTree = ({dict, depth=0, checkedInit=false, passInSetCheckedRecur=null, passInGetCheckedRecur=null, refreshTop=null}) => {
+  const [checked, setChecked] = useState(checkedInit)
+
+  let childSetChecked = []
+  let childGetChecked = []
+
+  const setParentChildSetChecked = (setter) => {
+    childSetChecked.push(setter)
+  }
+  const setParentChildGetChecked = (getter) => {
+    childGetChecked.push(getter)
+  }
+  
+  const calculateInd = () => {
+    if (childGetChecked.length > 0) {
+      let s = childGetChecked.reduce((acc, curr) => curr() === 'ind' ? 'ind' : (acc === curr() ? acc : 'ind'), childGetChecked[0]())
+      // TODO: can i do setChecked here?
+      setChecked(s)
+      return s
+    }
+
+    return checked
+  }
+
+  if (refreshTop === null) {
+    refreshTop = () => {
+      if (childGetChecked !== null) {
+        calculateInd()
+      }
+    }
+  }
+
+  const handlePress = (e) => {
+    setChecked(e.target.checked ? 'checked' : 'unchecked')
+    if (childSetChecked !== null) {
+      childSetChecked.forEach(it => it(e.target.checked ? 'checked' : 'unchecked'))
+      // childSetChecked()
+    }
+    refreshTop()
+  }
+
+  let entries = []
+  let i = 0
+
+  for (const d of dict.content) {
+    entries.push(
+      <StructuredDictAsTree key={i} dict={d} depth={depth+1} checkedInit={checked} passInSetCheckedRecur={setParentChildSetChecked} passInGetCheckedRecur={setParentChildGetChecked} refreshTop={refreshTop}/>
+    )
+    i += 1
+  }
+
+  if (passInSetCheckedRecur !== null) {
+    passInSetCheckedRecur((s) => {setChecked(s); childSetChecked.forEach(it => it(s))})
+  }
+  if (passInGetCheckedRecur !== null) {
+    if (childGetChecked.length > 0) {
+      passInGetCheckedRecur(calculateInd)
+    } else {
+      passInGetCheckedRecur(() => checked) // TODO: maybe this is being captured by value, might need to wrap in array
+    }
+  }
+
+  
   return (
-    <div>
-      {Object.entries(dict).map((key, value) => {
-        <Box sx={{ display: 'flex', flexDirection: 'column', ml: 3*depth }}>
-          hi
-          <FormControlLabel
-          label={"HEY"}
-          control={<Checkbox />}
-          />
-          {/* <DictToTree dict={value} depth={depth+1} /> */}
-        </Box>
-      })}
-    </div>
+    <>
+      <Box sx={{ display: 'flex', flexDirection: 'column', ml: 3*depth }}>
+        <FormControlLabel
+        label={dict.name}
+        control={<Checkbox checked={checked === 'checked'} indeterminate={checked === 'ind'} onChange={handlePress}/>}
+        />
+        {/* <DictToTree dict={value} depth={depth+1} /> */}
+        {entries}
+      </Box>
+    </>
   )
-
-  return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', ml: 3*depth }}>
-    {/* <FormControlLabel
-      label="Child 1"
-      control={<Checkbox checked={checked[0]} onChange={handleChange2} />}
-    />
-    <FormControlLabel
-      label="Child 2"
-      control={<Checkbox checked={checked[1]} onChange={handleChange3} />}
-    /> */}
-    {Object.entries(dict).map((key, value) => {
-      <FormControlLabel
-        label={key}
-        control={<Checkbox />}
-      />
-    })}
-    <DictToTree dict={label} depth={depth+1} />
-  </Box>
-  )
-
 }
 
 const CreatePresetModal = ({ onClose }) => {
   const [name, setName] = useState('name')
+
+  const status = useStatusStore(s => s.status)
 
   const savePreset = () => {
     console.log("savePreset")
@@ -110,6 +183,9 @@ const CreatePresetModal = ({ onClose }) => {
       },
     }
   }
+
+  const structuredDict = buildTreeDict(status)
+
   
   return (
     <Modal onClose={onClose}>
@@ -125,10 +201,8 @@ const CreatePresetModal = ({ onClose }) => {
           </div>
 
 
-          <DictToTree dict={testDict}/>
-          {/* <FormGroup>
-            <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
-          </FormGroup> */}
+          {/* <DictToTree dict={testDict}/> */}
+          <StructuredDictAsTree dict={structuredDict} checkedInit={'unchecked'}/>
         </div>
       </Card>
     </Modal>
