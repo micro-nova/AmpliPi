51bcd3b3e729c23fc551d18a0d4d1d0abf241729
Lincoln Lorenz <lincoln@micro-nova.com>
2023-05-01 12:25:17 -0400

    Remove missing module

--- amplipi/display/__init__.py
+++ amplipi/display/__init__.py
@@ -1,2 +1,2 @@
 """ Display """
-__all__ = ["common", "display", "einkdisplay", "epd2in13_V3", "epdconfig", "tftdisplay"]
+__all__ = ["common", "display", "einkdisplay", "epd2in13_V3", "tftdisplay"]
