78b32a1a154d3fa79335bd2f840ed9ab84f97f91
Klayton Smith II <klayton@micro-nova.com>
2023-11-01 12:30:52 -0400

    removed symlink and changed paths in webapp

--- web/src/utils/getIcon.jsx
+++ web/src/utils/getIcon.jsx
@@ -1,50 +1,50 @@
-import spotify from "@/assets/spotify.png";
-import dlna from "@/assets/dlna.png";
-import bluetooth from "@/assets/bluetooth.png";
-import fmradio from "@/assets/fmradio.png";
-import shairport from "@/assets/shairport.png";
-import pandora from "@/assets/pandora.png";
-import plexamp from "@/assets/plexamp.png";
-import lms from "@/assets/lms.png";
-import internetradio from "@/assets/internet_radio.png";
-import rca from "@/assets/rca_inputs.jpg";
+import spotify from "@/../static/imgs/spotify.png";
+import dlna from "@/../static/imgs/dlna.png";
+import bluetooth from "@/../static/imgs/bluetooth.png";
+import fmradio from "@/../static/imgs/fmradio.png";
+import shairport from "@/../static/imgs/shairport.png";
+import pandora from "@/../static/imgs/pandora.png";
+import plexamp from "@/../static/imgs/plexamp.png";
+import lms from "@/../static/imgs/lms.png";
+import internetradio from "@/../static/imgs/internet_radio.png";
+import rca from "@/../static/imgs/rca_inputs.jpg";
 
 export const getIcon = (type) => {
-    if (type === null || type === undefined) {
-        return internetradio;
-    }
-    switch (type.toUpperCase()) {
+  if (type === null || type === undefined) {
+    return internetradio;
+  }
+  switch (type.toUpperCase()) {
     case "SPOTIFY":
-        return spotify;
+      return spotify;
 
     case "DLNA":
-        return dlna;
+      return dlna;
 
     case "BLUETOOTH":
-        return bluetooth;
+      return bluetooth;
 
     case "FM RADIO":
-        return fmradio;
+      return fmradio;
 
     case "AIRPLAY":
-        return shairport;
+      return shairport;
 
     case "PANDORA":
-        return pandora;
+      return pandora;
 
     case "PLEXAMP":
-        return plexamp;
+      return plexamp;
 
     case "LMS":
-        return lms;
+      return lms;
 
     case "RCA":
-        return rca;
+      return rca;
 
     case "INTERNET RADIO":
-        return internetradio;
+      return internetradio;
 
     default:
-        return internetradio;
-    }
+      return internetradio;
+  }
 };
