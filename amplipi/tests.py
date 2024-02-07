f095a5587001f3d2971e4492bbd422d0c9281e10
Lincoln Lorenz <lincoln@micro-nova.com>
2024-02-07 17:03:30 -0500

    Shorten preamp test audio files

--- amplipi/tests.py
+++ amplipi/tests.py
@@ -266,13 +266,15 @@ def preamp_test(ap1: Client, exp_unit: bool = False):
   presets = [pst for pst in status.presets if pst.name.startswith('preamp-analog-in-') and pst.id is not None]
   try_analog = not exp_unit  # the analog tester is not needed for expansion units
   if try_analog and ap2.available():
-    print('Test will play Analog 1 Left, Analog 1 Right...Analog 4 Right, Digital 1 Left... Digital 4 Right')
-    print('- Verify that each side and all 8 sources are played out of each of the 6 zones')
+    print('Test will play the following in a loop:')
+    print('- 1L, 1R, 2L ... 4R in a female voice for analog inputs')
+    print('- 1L, 1R, 2L ... 4R in a male voice for digital inputs')
+    print('Verify that each side and all 8 sources are played out of each of the 6 zones')
   else:
     if try_analog:
       print('No analog tester found at aptestanalog.local, only able to test digital inputs\n')
-    print('Test will play Digital 1 Left... Digital 4 Right')
-    print('- Verify that each side and all 4 sources are played out of each of the 6 zones')
+    print('Test will play 1L, 1R, 2L ... 4R')
+    print('- Verify that each side and all 4 digital sources are played out of each of the 6 zones')
   digital_msgs = [models.Announcement(source_id=src, media=f'web/static/audio/digital{src+1}.mp3', vol=-30)
                   for src in range(4)]
   analog_msgs = [models.Announcement(source_id=src, media=f'web/static/audio/analog{src+1}.mp3') for src in range(4)]
