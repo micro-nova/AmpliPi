274c965b61fa42932ddc594917f3dfac6334eb62
Steven Engelbert <steve@micro-nova.com>
2024-03-25 14:16:00 -0400

    Add functionality to edit Pandora metadata file on like to flip the liked enum properly

--- amplipi/models.py
+++ amplipi/models.py
@@ -135,7 +135,7 @@ class SourceInfo(BaseModel):
   station: Optional[str]  # name of radio station
   img_url: Optional[str]
   supported_cmds: List[str] = []
-  rating: Optional[int] = 0
+  rating: Optional[int]
   # "rating" is a pandora-specific enum
   # decode where text is common wording and ("text") is the pandora/pianobar terminology
   # 0 default
--- web/src/components/MediaControl/MediaControl.jsx
+++ web/src/components/MediaControl/MediaControl.jsx
@@ -24,16 +24,7 @@ const MediaControl = ({ selectedSource }) => {
     // const source = status.sources[selectedSource]
     const source = useStatusStore((s) => s.status.sources[selectedSource]);
     const meta = useStatusStore((s) => s.status.sources[selectedSource].info);
-
-    const [invertRating, setInverse] = React.useState(false); // Stores the clicked state of a song, that way it can be set to have a green thumb if the like is clicked as that metadata won't update in the backend until the song is played again
-    const [currentSong, setSong] = React.useState();
-
-    React.useEffect(() => {
-        if(currentSong != String(meta.track)){
-            setInverse(false);
-            setSong(String(meta.track));
-        }
-    }, [meta])
+    console.log(meta);
 
     const enabled = "media-control media-control-enabled";
     const disabled = "media-control media-control-disabled";
@@ -120,15 +111,8 @@ const MediaControl = ({ selectedSource }) => {
 
     function Ban() {
         if(isSupported("ban")){
-            let thumbColor;
-            if(meta.rating == 2){
-                thumbColor = "#FF0000";
-            } else {
-                thumbColor = "#FFFFFF";
-            }
             return(
                 <FontAwesomeIcon
-                    style={{ color: thumbColor, transition: 'color 5s' }}
                     icon={faThumbsDown}
                     className={cmdToClassName("ban")}
                     onClick={() => {postCommand("ban");}}
@@ -141,24 +125,16 @@ const MediaControl = ({ selectedSource }) => {
         if(isSupported("love")){
             let thumbColor;
             if(meta.rating != 1){
-                if(invertRating){
-                    thumbColor = "#00FF00";
-                } else {
-                    thumbColor = "#FFFFFF";
-                }
+                thumbColor = "#FFFFFF";
             } else {
-                if(invertRating){
-                    thumbColor = "#FFFFFF";
-                } else {
-                    thumbColor = "#00FF00";
-                }
+                thumbColor = "#00FF00";
             }
             return(
                 <FontAwesomeIcon
                     style={{ color: thumbColor, transition: 'color 5s' }}
                     icon={faThumbsUp}
                     className={cmdToClassName("love")}
-                    onClick={() => {postCommand("love"); setInverse(!invertRating);}}
+                    onClick={() => postCommand("love")}
                 />
             )
         }
