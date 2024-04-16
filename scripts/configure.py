565359e34863cfc99150c7c2d3bbd4c0c7431fb2
Steven Engelbert <steve@micro-nova.com>
2024-04-16 16:42:11 -0400

    Add rules to configure.py
    clean udisks2-listener.sh to remove unneeded line about permissions, reword comment

--- scripts/configure.py
+++ scripts/configure.py
@@ -169,6 +169,12 @@ _os_deps: Dict[str, Dict[str, Any]] = {
       'sudo dpkg -i /tmp/squeezelite_1.9.9-1449_armhf.deb',
       'sudo systemctl stop squeezelite',
       'sudo systemctl disable squeezelite',
+
+      'sudo cp scripts/udisks2-listener.sh /usr/local/bin',
+      'sudo cp scripts/edit_media_directories.py /usr/local/bin',
+      'sudo cp scripts/udisks2-listener.service /etc/systemd/system'
+      'sudo systemctl enable udisks2-listener.service',
+      'sudo systemctl start udisks2-listener.service',
     ]
   },
   'dlna' : {
