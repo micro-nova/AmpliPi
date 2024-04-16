cdf62c234ec8d3d8dac5a8629e190ebc0f2d9611
Steven Engelbert <steve@micro-nova.com>
2024-04-16 16:42:11 -0400

    Add rules to configure.py
    clean udisks2-listener.sh to remove unneeded line about permissions, reword comment

--- scripts/udisks2-listener.sh
+++ scripts/udisks2-listener.sh
@@ -3,9 +3,7 @@
 
 dbus-monitor --system "interface='org.freedesktop.UDisks2.Filesystem'" |
 while read -r line; do
-  # Execute your bash script when a drive is mounted
-  sudo chmod 766 /var/lib/squeezeboxserver/prefs/server.prefs
-
+  # Execute when a drive is mounted
   source /home/pi/amplipi-dev/venv/bin/activate
 
   python /usr/local/bin/edit_media_directories.py
