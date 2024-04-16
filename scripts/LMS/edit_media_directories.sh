ed76dbafdb4016a4a6a1adbe9dd393ee153a95f0
Steven Engelbert <steve@micro-nova.com>
2024-04-16 13:33:35 -0400

    Add local files to version control

---
+++ scripts/LMS/edit_media_directories.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#/usr/local/bin
+
+sudo chmod 766 /var/lib/squeezeboxserver/prefs/server.prefs
+
+source /home/pi/amplipi-dev/venv/bin/activate
+
+python /usr/local/bin/edit_media_directories.py
+
+/usr/sbin/squeezeboxserver-scanner
