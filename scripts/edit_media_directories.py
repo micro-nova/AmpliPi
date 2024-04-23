fb141df47755cd187f19dfefc2085f9d6f54369f
Steven Engelbert <steve@micro-nova.com>
2024-04-23 15:19:36 -0400

    Add logging support to edit_media_directories.py

--- scripts/edit_media_directories.py
+++ scripts/edit_media_directories.py
@@ -3,20 +3,24 @@
 """A program for automatically adding and removing removable drives to the LMS media drives list"""
 import psutil
 import requests
+import logging
+import sys
 
-def get_usb_drives():
+def get_usb_drives(logger: logging.Logger):
   """Reads the mount point for removable drives, returns them in a list"""
   usb_drives = []
+  logger.debug("Finding mounted USB devices")
   for partition in psutil.disk_partitions():
     # Exclude rootfs, the backup drive that happens to mount to /media/pi
     if '/media/pi' in partition.mountpoint and not 'rootfs' in partition.mountpoint:
       usb_drives.append(str(partition.mountpoint))
+      logger.info(f"Found USB device at: {partition.mountpoint}")
   return usb_drives
 
 
-def edit_directories():
+def edit_directories(logger: logging.Logger):
   """Update LMS server with list of drives"""
-  usb_drives = get_usb_drives() # Get list of available drives
+  usb_drives = get_usb_drives(logger) # Get list of available drives
   usb_drives.append("") # Add blank drive to reflect empty string that is always at the end of the mediadirs section of request body
   url = 'http://localhost:9000/settings/server/basic.html'
   data={
@@ -32,9 +36,16 @@ def edit_directories():
     data[f"pref_mediadirs{d}"] = drive
     data[f"pref_ignoreInAudioScan{d}"] = 1
 
+  logger.info("Adding drives to LMS settings...")
   requests.post(url, data=data, verify=False, timeout=5)
+  logger.info("Drives added, LMS scanning now.")
 
 
 
 if __name__ == "__main__":
-  edit_directories()
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.DEBUG)
+  sh = logging.StreamHandler(sys.stdout)
+  logger.addHandler(sh)
+
+  edit_directories(logger)
