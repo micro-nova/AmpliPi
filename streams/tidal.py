6c82c74bd599f7d29b7132b98e53c13d3444f909
Steven Engelbert <steve@micro-nova.com>
2024-04-26 13:17:37 -0400

    Increase functionality of tidal api (which is made with tidalapi)

--- streams/tidal.py
+++ streams/tidal.py
@@ -1,7 +1,11 @@
-import tidalapi
+import tidalapi # Note pydantic objects below before importing anything more specific from this package
+from tidalapi.page import PageItem, PageLink
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional, Union
 import uvicorn
 import json
 
@@ -17,26 +21,81 @@ app.add_middleware(
 
 tidal = tidalapi.Session()
 
-def save_session():
+# Pydantic Objects to reflect a pared down version of tidalapi objects, but in a way that you can send back to the frontend
+class Playlist(BaseModel):
+  type = "Playlist"
+  name: str
+  id: str
+  art: Optional[str]
+
+class Mix(BaseModel):
+  type = "Mix"
+  name: str
+  id: str
+  art: Optional[str]
+
+class Page(BaseModel):
+  type = "Page"
+  name: str
+  path: Optional[str]
+  art: Optional[str]
+
+class Track(BaseModel):
+  type = "Track"
+  name: str
+  id: str
+  art: Optional[str]
+  duration: Optional[str]
+
+class Album(BaseModel):
+  type = "Album"
+  name: str
+  id: str
+  artists: Optional[Union[str, list]]
+  art: Optional[str]
+  tracks: Optional[dict[str, Track]]
+
+
+class HomeScreen(BaseModel):
+  title: str
+  items: dict[str, Page, Album, Track, Mix, Playlist]
+
+
+def save_session(): # TODO: Make file saving more secure, I don't want unencrypted tokens sitting around
   """Save session data to a file"""
   with open("file.json", "w", encoding="utf-8") as f:
     json.dump({
       "token_type": tidal.token_type,
       "access_token": tidal.access_token,
       "refresh_token": tidal.refresh_token,
-      "expiry_time": tidal.expiry_time
     }, f)
 
 
 def load_session():
   """Load session data from a file"""
-  with open("file.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-    tidal.load_oauth_session(data["token_type"], data["access_token"], data["refresh_token"], data["expiry_time"])
+  try:
+    with open("file.json", "r", encoding="utf-8") as f:
+      data = json.load(f)
+      tidal.load_oauth_session(data["token_type"], data["access_token"], data["refresh_token"])
+    return True
+  except Exception:
+    return False
+
+def check_session():
+  if not tidal.access_token:
+    if not load_session():
+      return False
   return True
 
+def trackify_album(album_id: str) -> dict[Track]:
+  album = tidal.album(album_id)
+  tracks = {}
+  for track in album.tracks():
+    tracks[track.name] = Track(name=track.name, id=track.id, artist=track.artist, art=track.album.cover, duration=track.duration)
+  return tracks
 
-@app.get('/login')
+
+@app.get('/login/')
 def login():
   """Checks if the session is logged in, if not send forward the authentication link to automagically gather an authtoken from Tidal"""
   if tidal.check_login():
@@ -49,6 +108,44 @@ def login():
     else:
       login, future = tidal.login_oauth()
       return login.verification_uri_complete
+
+
+@app.get('/pages/home/')
+def get_default():
+  if not check_session():
+    return ("You must log in before you use this function, go to GET /login/ to do so.")
+
+  home = tidal.home()
+  home.categories.extend(tidal.explore().categories)
+  categories = {}
+  for category in home.categories:
+    items = {}
+    for item in category.items:
+      if isinstance(item, tidalapi.Album):
+        items[item.name] = Album(name=item.name, id=item.id, artists=item.artists, art=item.cover)
+
+      elif isinstance(item, tidalapi.Mix):
+        items[item.title] = Mix(name=item.title, id=item.id, art=item.images.medium)
+
+      elif isinstance(item, tidalapi.Playlist):
+        items[item.name] = Playlist(name=item.name, id=item.id, art=item.square_picture)
+
+      elif isinstance(item, tidalapi.Track):
+        items[item.name] = Track(name=item.name, id=item.id, artist=item.artist, art=item.album.cover, duration=item.duration)
+
+      elif isinstance(item, PageLink):
+        items[item.title] = Page(name=item.title, path=item.api_path, art=item.image_id)
+
+    categories[category.title] = HomeScreen(title=category.title, items=items)
+
+  return categories
+
+
+@app.get('/pages/album/{album_id}/')
+def get_category_by_name(album_id: str):
+  if not check_session():
+    return ("You must log in before you use this function, go to GET /login/ to do so.")
+  return trackify_album(album_id)
 
 
 if __name__ == "__main__":
