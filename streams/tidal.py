12628dac156380bc420b06ff7c88e3093cc2f3fe
Steven Engelbert <steve@micro-nova.com>
2024-04-26 14:19:48 -0400

    Add typehinting, filtration, enum for filtration

--- streams/tidal.py
+++ streams/tidal.py
@@ -5,9 +5,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Optional, Union
+from typing import Optional, Union, Dict
 import uvicorn
 import json
+from enum import Enum
 
 app = FastAPI()
 
@@ -22,6 +23,14 @@ app.add_middleware(
 tidal = tidalapi.Session()
 
 # Pydantic Objects to reflect a pared down version of tidalapi objects, but in a way that you can send back to the frontend
+
+class Filtration_Query(Enum):
+  ALBUM = "album"
+  MIX = "mix"
+  PLAYLIST = "playlist"
+  TRACK = "track"
+  PAGE = "page"
+
 class Playlist(BaseModel):
   type = "Playlist"
   name: str
@@ -71,7 +80,7 @@ def save_session(): # TODO: Make file saving more secure, I don't want unencrypt
     }, f)
 
 
-def load_session():
+def load_session() -> bool:
   """Load session data from a file"""
   try:
     with open("file.json", "r", encoding="utf-8") as f:
@@ -81,10 +90,10 @@ def load_session():
   except Exception:
     return False
 
-def check_session():
-  if not tidal.access_token:
-    if not load_session():
-      return False
+def check_session() -> bool:
+  """Checks if the session is logged in, or if it's loadable from a file, and returns a bool"""
+  if not tidal.check_login():
+    return load_session()
   return True
 
 def trackify_album(album_id: str) -> dict[Track]:
@@ -101,17 +110,18 @@ def login():
   if tidal.check_login():
     save_session()
     return "Session already logged in"
+
+  if load_session():
+    save_session()
+    return "Session loaded"
   else:
-    if load_session():
-      save_session()
-      return "Session loaded"
-    else:
-      login, future = tidal.login_oauth()
-      return login.verification_uri_complete
+    login, future = tidal.login_oauth()
+    return login.verification_uri_complete
 
 
 @app.get('/pages/home/')
-def get_default():
+def get_homepage() -> Union[str, Dict[str, HomeScreen]]:
+  """Gets all data from the homepage"""
   if not check_session():
     return ("You must log in before you use this function, go to GET /login/ to do so.")
 
@@ -141,8 +151,42 @@ def get_default():
   return categories
 
 
+@app.get('/pages/home/{filter_query}/')
+def get_filtered_homepage(filter_query: Filtration_Query) -> Union[str, Dict[str, HomeScreen]]:
+  """Gets all data from the homepage, filtered to a single data type"""
+  if not check_session():
+    return ("You must log in before you use this function, go to GET /login/ to do so.")
+
+  filter_query = filter_query.value.lower()
+
+  home = tidal.home()
+  home.categories.extend(tidal.explore().categories)
+  categories = {}
+  for category in home.categories:
+    items = {}
+    for item in category.items:
+      if isinstance(item, tidalapi.Album) and filter_query == "album":
+        items[item.name] = Album(name=item.name, id=item.id, artists=item.artists, art=item.cover)
+
+      elif isinstance(item, tidalapi.Mix) and filter_query == "mix":
+        items[item.title] = Mix(name=item.title, id=item.id, art=item.images.medium)
+
+      elif isinstance(item, tidalapi.Playlist) and filter_query == "playlist":
+        items[item.name] = Playlist(name=item.name, id=item.id, art=item.square_picture)
+
+      elif isinstance(item, tidalapi.Track) and filter_query == "track":
+        items[item.name] = Track(name=item.name, id=item.id, artist=item.artist, art=item.album.cover, duration=item.duration)
+
+      elif isinstance(item, PageLink) and filter_query == "page":
+        items[item.title] = Page(name=item.title, path=item.api_path, art=item.image_id)
+
+    categories[category.title] = HomeScreen(title=category.title, items=items)
+
+  return categories
+
+
 @app.get('/pages/album/{album_id}/')
-def get_category_by_name(album_id: str):
+def get_album_tracks(album_id: str) -> Dict[str, Track]:
   if not check_session():
     return ("You must log in before you use this function, go to GET /login/ to do so.")
   return trackify_album(album_id)
