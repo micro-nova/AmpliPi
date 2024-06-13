a296d1a3c38a371f93965735dcab97f0516d7779
Steven Engelbert <steve@micro-nova.com>
2024-06-13 15:16:03 -0400

    Reduce state, increase documentation, improve variable names in CustomMarquee

--- web/src/components/CustomMarquee/CustomMarquee.jsx
+++ web/src/components/CustomMarquee/CustomMarquee.jsx
@@ -11,33 +11,38 @@ function CustomMarquee(props) {
     const resizeCooldown = React.useRef(false);
 
     const childrenRef = React.useRef(null);
-    const [childrenScroll, setChildScroll] = React.useState(false);
-    const childrenOverrideTimeout = React.useRef(null);
-    const childrenScrollOverride = React.useRef(true); // set to true by defualt so that there is no scrolling on load, until a setTimeout later on changes that 2 seconds after load
-
-    React.useEffect(() => {assessMarquee();}, [children]);
+    const [marqueeScroll, setScroll] = React.useState(false);
+    const scrollPause = React.useRef(null);
 
     function assessMarquee() {
+        // This function is called in two cases:
+        // 1. a marquee scroll cycle has completed
+        // 2. the screen was resized
         const container = containerRef.current;
         const scroll = childrenRef.current;
 
-        if(container && scroll){
-            if(container.offsetWidth < scroll.scrollWidth && !childrenScrollOverride.current){
-                setChildScroll(true);
-            } else if(container.offsetWidth >= scroll.scrollWidth && childrenScrollOverride.current){
-                clearTimeout(childrenOverrideTimeout.current);
+        if(container && scroll){ // Only start checking after pageload
+
+            // This if statement is watching two states:
+            // 1. The screen size (more specifically, the size of the text vs size of the div it's inside of)
+            // 2. A boolean
+            //
+            // The screen size is set based on the device using the UI and/or the browser window size
+            // The boolean is set to true by the timer within the if statement, and set to false whenever a marquee scroll cycle completes
+
+            if(container.offsetWidth < scroll.scrollWidth && !marqueeScroll){
+                // If the content does not fit the div, and if the scroll is not paused, start scrolling after a two second timer
+                // The timer is used to create a pause effect, keep the data from simply scrolling infinitely
+                scrollPause.current = setTimeout(()=>{setScroll(true)}, 2000);
+            } else {
+                // if content is not meant to scroll, remove the timer that allows it to start scrolling again
+                // This is useful in cases when the screen is resized or the song changes during a pause phase
+                clearTimeout(scrollPause.current);
             }
         }
     }
 
-    function setOverride(){
-        setChildScroll(false);
-        childrenScrollOverride.current = true;
-
-        childrenOverrideTimeout.current = setTimeout(()=>{childrenScrollOverride.current = false; assessMarquee();}, 2000); // When a scroll completes, pause for two seconds and restart (unless the timeout is cleared)
-    }
-
-    setTimeout(()=>{childrenScrollOverride.current = false; assessMarquee();}, 2000) // used to kick off useEffect after two seconds of page load
+    setTimeout(()=>{assessMarquee();}, 2000) // used to kick off useEffect after two seconds of page load
 
 
     function handleResize(){
@@ -53,7 +58,7 @@ function CustomMarquee(props) {
 
 
     return(
-        <Marquee play={childrenScroll} speed={30} onCycleComplete={() => {setOverride()}}>
+        <Marquee play={marqueeScroll} speed={30} onCycleComplete={() => {setScroll(false); assessMarquee();}}>
             <div
                 style={{marginLeft: "10px"}} //Needs inline styling for margin to add a gap between start and end of marquee, also keeps marquee from "overscrolling" (stopping a pixel or two after reaching the wraparound point due to minor lag)
                 className="marquee-text"
