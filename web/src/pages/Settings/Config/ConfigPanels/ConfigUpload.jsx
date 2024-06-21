b1b2258ce0ea1b6e18ce2fa49499343543435f40
Steven Engelbert <steve@micro-nova.com>
2024-06-21 16:11:31 -0400

    Reduce props in Config components

--- web/src/pages/Settings/Config/ConfigPanels/ConfigUpload.jsx
+++ web/src/pages/Settings/Config/ConfigPanels/ConfigUpload.jsx
@@ -1,12 +1,14 @@
 
 import React from 'react';
 import "../Config.scss";
-import ConfigPanel from './ConfigPanel.jsx';
+import ConfigPanel from './ConfigTemplates/ConfigPanel.jsx';
 import Button from '@mui/material/Button/Button';
+import ConfigModal from './ConfigTemplates/ConfigModal';
 
 export default function ConfigDownload(){
     const [file, setFile] = React.useState([]);
     const [filePicked, setFilePicked] = React.useState(false);
+    const [modalOpen, setModalOpen] = React.useState(false);
 
     const UploadConfig = () => {
         return fetch("/api/load", {
@@ -16,42 +18,40 @@ export default function ConfigDownload(){
         });
     };
 
-    function Contents(props) {
-        const { onClick } = props;
+    const onChange = (event) => {
+        if (!event.target.files[0]) {
+            setFilePicked(false);
+            return;
+        }
+        event.target.files[0].text().then((text) => {
+            setFile(JSON.parse(text));
+        });
+        setFilePicked(true);
+    };
 
-        const onChange = (event) => {
-            if (!event.target.files[0]) {
-                setFilePicked(false);
-                return;
-            }
-            event.target.files[0].text().then((text) => {
-                setFile(JSON.parse(text));
-            });
-            setFilePicked(true);
-        };
-
-        return(
-            <>
+    return(
+        <>
+            <ConfigPanel
+                title={"Upload Config"}
+                subheader={"Uploads the selected configuration file."}
+                successText={"Config uploaded successfully!"}
+            >
                 <input
                     type="file"
                     accept=".json,application/json"
                     onChange={onChange}
                 />
-                <Button disabled={!filePicked} onClick={onClick}>
+                <Button disabled={!filePicked} onClick={() => {setModalOpen(true);}}>
                     Upload
                 </Button>
-            </>
-        )
-    }
+            </ConfigPanel>
 
-    return(
-        <ConfigPanel
-            title={"Upload Config"}
-            subheader={"Uploads the selected configuration file."}
-            handler={UploadConfig}
-            Contents={Contents}
-            modalBody={"This will replace the previous config, ensure you've downloaded the current config if you wish to keep it!"}
-            successText={"Config uploaded successfully!"}
-        />
+            <ConfigModal
+                body={"This will replace the previous config, ensure you've downloaded the current config if you wish to keep it!"}
+                confirm={() => {UploadConfig();}}
+                open={modalOpen}
+                setOpen={setModalOpen}
+            />
+        </>
     )
 };
