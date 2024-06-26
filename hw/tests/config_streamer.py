40856eac095e1eb76c1b7238680aeb2a061fd651
Ryan Tasson <ryan@micro-nova.com>
2024-06-26 11:03:04 -0400

    4 spaces -> 2 spaces

--- hw/tests/config_streamer.py
+++ hw/tests/config_streamer.py
@@ -17,55 +17,55 @@ BOARD_REV_RE = re.compile(r"(\d+)([A-z])")
 
 
 def main() -> None:
-    """Write board info to eeprom, using user input for serial number and board revision"""
-    serial: Optional[int] = None
-    board_rev: Union[Tuple[int, str], Tuple[None, None]] = (None, None)
-    try:
-        serial_input = input("Enter streamer serial number (ie: 1234):")
-        serial = int(serial_input)
-        board_rev_input = input(
-            "Enter streamer board revision (ie: 1a):").strip()
-        board_rev_match = BOARD_REV_RE.fullmatch(board_rev_input)
-        if board_rev_match and len(board_rev_match.groups()) == 2:
-            board_rev = (int(board_rev_match.group(1)),
-                         board_rev_match.group(2))
-        else:
-            print(f'Failed to parse board revision from "{board_rev_input}"')
-    except ValueError as e:
-        print(f'Failed to read serial number from "{serial_input}": {e}')
-    except (KeyboardInterrupt, EOFError):
-        print("Failed to read input")
+  """Write board info to eeprom, using user input for serial number and board revision"""
+  serial: Optional[int] = None
+  board_rev: Union[Tuple[int, str], Tuple[None, None]] = (None, None)
+  try:
+    serial_input = input("Enter streamer serial number (ie: 1234):")
+    serial = int(serial_input)
+    board_rev_input = input(
+        "Enter streamer board revision (ie: 1a):").strip()
+    board_rev_match = BOARD_REV_RE.fullmatch(board_rev_input)
+    if board_rev_match and len(board_rev_match.groups()) == 2:
+      board_rev = (int(board_rev_match.group(1)),
+                   board_rev_match.group(2))
+    else:
+      print(f'Failed to parse board revision from "{board_rev_input}"')
+  except ValueError as e:
+    print(f'Failed to read serial number from "{serial_input}": {e}')
+  except (KeyboardInterrupt, EOFError):
+    print("Failed to read input")
 
-    if serial is None or board_rev[0] is None:
-        sys.exit(1)
+  if serial is None or board_rev[0] is None:
+    sys.exit(1)
 
-    try:
-        print(
-            f"Writing streamer config to eeprom: serial={serial}, board_rev={board_rev[0]}{board_rev[1]}")
-        eeprom = EEPROM(BoardType.STREAMER_SUPPORT)
+  try:
+    print(
+        f"Writing streamer config to eeprom: serial={serial}, board_rev={board_rev[0]}{board_rev[1]}")
+    eeprom = EEPROM(BoardType.STREAMER_SUPPORT)
 
-        br = BoardRev(board_rev[0], board_rev[1])
-        if serial == -1:
-            eeprom.erase()
-        else:
-            eeprom.write_board_info(BoardInfo(serial=serial, unit_type=UnitType.STREAMER,
-                                    board_type=BoardType.STREAMER_SUPPORT, board_rev=br))
-    except Exception as e:
-        print(f"Failed to write board info to eeprom: {e}")
-        sys.exit(1)
+    br = BoardRev(board_rev[0], board_rev[1])
+    if serial == -1:
+      eeprom.erase()
+    else:
+      eeprom.write_board_info(BoardInfo(serial=serial, unit_type=UnitType.STREAMER,
+                              board_type=BoardType.STREAMER_SUPPORT, board_rev=br))
+  except Exception as e:
+    print(f"Failed to write board info to eeprom: {e}")
+    sys.exit(1)
 
-    try:
-        print("Resetting amplipi to detect streamer capability")
-        response = requests.post(
-            'http://0.0.0.0/api/factory_reset', timeout=10)
-        if not response.ok:
-            print(f"Failed to reset amplipi: {response.text}")
-    except Exception as e:
-        print(f"Failed to reset amplipi: {e}")
+  try:
+    print("Resetting amplipi to detect streamer capability")
+    response = requests.post(
+        'http://0.0.0.0/api/factory_reset', timeout=10)
+    if not response.ok:
+      print(f"Failed to reset amplipi: {response.text}")
+  except Exception as e:
+    print(f"Failed to reset amplipi: {e}")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        print(main.__doc__)
-    else:
-        main()
+  if len(sys.argv) > 1:
+    print(main.__doc__)
+  else:
+    main()
--- hw/tests/peak_detect.py
+++ hw/tests/peak_detect.py
@@ -19,77 +19,77 @@ SAMPLE_RATE = 20.0    # Hz, rate at which to sample all channels
 
 
 def cs_setup() -> None:
-    """ CS pin setup. Leaves CS inactive. """
-    if not Path('/sys/class/gpio/gpio17').exists():
-        with open('/sys/class/gpio/export', 'w') as file:
-            file.write(f'{CS_PIN}')
-        time.sleep(0.1)
-        with open('/sys/class/gpio/gpio17/direction', 'w') as file:
-            file.write('out')
+  """ CS pin setup. Leaves CS inactive. """
+  if not Path('/sys/class/gpio/gpio17').exists():
+    with open('/sys/class/gpio/export', 'w') as file:
+      file.write(f'{CS_PIN}')
+    time.sleep(0.1)
+    with open('/sys/class/gpio/gpio17/direction', 'w') as file:
+      file.write('out')
 
-    with open(f'/sys/class/gpio/gpio{CS_PIN}/value', 'w') as cs_file:
-        cs_file.write('1')
+  with open(f'/sys/class/gpio/gpio{CS_PIN}/value', 'w') as cs_file:
+    cs_file.write('1')
 
 
 def read_adc(spi: busio.SPI, channel: int) -> int:
-    """ Read a single ADC channel of the MCP3008  """
-    # TODO: This is still kinda slow, investigate Python's 'spidev'
-    # library or https://iosoft.blog/2020/06/11/fast-data-capture-raspberry-pi/
-    rx_buf = bytearray(3)
-    tx_buf = bytearray((0x01, 0x80 | (channel << 4), 0x00))
-    with open(f'/sys/class/gpio/gpio{CS_PIN}/value', 'w') as cs_file:
-        cs_file.write('0')
-    spi.write_readinto(tx_buf, rx_buf)
-    with open(f'/sys/class/gpio/gpio{CS_PIN}/value', 'w') as cs_file:
-        cs_file.write('1')
-    raw_val = ((rx_buf[1] & 0x3) << 8) | rx_buf[2]
-    return raw_val
+  """ Read a single ADC channel of the MCP3008  """
+  # TODO: This is still kinda slow, investigate Python's 'spidev'
+  # library or https://iosoft.blog/2020/06/11/fast-data-capture-raspberry-pi/
+  rx_buf = bytearray(3)
+  tx_buf = bytearray((0x01, 0x80 | (channel << 4), 0x00))
+  with open(f'/sys/class/gpio/gpio{CS_PIN}/value', 'w') as cs_file:
+    cs_file.write('0')
+  spi.write_readinto(tx_buf, rx_buf)
+  with open(f'/sys/class/gpio/gpio{CS_PIN}/value', 'w') as cs_file:
+    cs_file.write('1')
+  raw_val = ((rx_buf[1] & 0x3) << 8) | rx_buf[2]
+  return raw_val
 
 
 def read_vals():
-    """ Read all 8 peak-detect channels forever. Every second, prints the max
-        value read for each channel over the preceding second.
-    """
-    # Setup SPI bus using hardware SPI:
-    peak_spi = busio.SPI(clock=board.SCLK_1,
-                         MOSI=board.MOSI_1, MISO=board.MISO_1)
-    while not peak_spi.try_lock():
-        pass
-    peak_spi.configure(baudrate=SPI_BAUD)
+  """ Read all 8 peak-detect channels forever. Every second, prints the max
+      value read for each channel over the preceding second.
+  """
+  # Setup SPI bus using hardware SPI:
+  peak_spi = busio.SPI(clock=board.SCLK_1,
+                       MOSI=board.MOSI_1, MISO=board.MISO_1)
+  while not peak_spi.try_lock():
+    pass
+  peak_spi.configure(baudrate=SPI_BAUD)
 
-    cs_setup()
+  cs_setup()
 
-    sample_period = 1 / SAMPLE_RATE
-    i = 0
-    max_val = [0] * 8
-    next_time = time.time()
-    while True:
-        for chan in range(8):
-            raw_val = read_adc(peak_spi, chan)
-            adc_val = raw_val * 100.0 / 1023.0
-            if adc_val > max_val[7 - chan]:
-                max_val[7 - chan] = adc_val
-                # filt_val = alpha*adc_val + (1-alpha)*filt_val
+  sample_period = 1 / SAMPLE_RATE
+  i = 0
+  max_val = [0] * 8
+  next_time = time.time()
+  while True:
+    for chan in range(8):
+      raw_val = read_adc(peak_spi, chan)
+      adc_val = raw_val * 100.0 / 1023.0
+      if adc_val > max_val[7 - chan]:
+        max_val[7 - chan] = adc_val
+        # filt_val = alpha*adc_val + (1-alpha)*filt_val
 
-        # Print header once every 30 seconds
-        if i % (30 * int(SAMPLE_RATE)) == 0:
-            print('  CH1R   CH1L   CH2R   CH2L   CH3R   CH3L   CH4R   CH4L')
+    # Print header once every 30 seconds
+    if i % (30 * int(SAMPLE_RATE)) == 0:
+      print('  CH1R   CH1L   CH2R   CH2L   CH3R   CH3L   CH4R   CH4L')
 
-        # Print vals once a second
-        if i % int(SAMPLE_RATE) == 0:
-            print(*(f'{val:5.1f}%' for val in max_val))
-            max_val = [0] * 8
-        i += 1
+    # Print vals once a second
+    if i % int(SAMPLE_RATE) == 0:
+      print(*(f'{val:5.1f}%' for val in max_val))
+      max_val = [0] * 8
+    i += 1
 
-        next_time = next_time + sample_period
-        sleep_time = next_time - time.time()
-        if sleep_time > 0:
-            time.sleep(sleep_time)
-        else:
-            print(f'Warning: ADC sampling took {-sleep_time}s too long!')
-            # Reset sampling period in case of NTP time jumps
-            next_time = time.time() + sample_period
+    next_time = next_time + sample_period
+    sleep_time = next_time - time.time()
+    if sleep_time > 0:
+      time.sleep(sleep_time)
+    else:
+      print(f'Warning: ADC sampling took {-sleep_time}s too long!')
+      # Reset sampling period in case of NTP time jumps
+      next_time = time.time() + sample_period
 
 
 if __name__ == "__main__":
-    read_vals()
+  read_vals()
